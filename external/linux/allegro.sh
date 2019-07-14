8296a6927cdbc81e86000120ef15502de77991b6
Ian T. Jacobsen <iantj92@gmail.com>
2019-07-14 20:49:57 +0200

    error in allegro.sh script

--- external/linux/allegro.sh
+++ external/linux/allegro.sh
@@ -3,7 +3,7 @@
 pushd $PWD/allegro5-4.4.3.1
     mkdir build
     cd build
-    cmake -DCMAKE_INSTALL_PREFIX=$PWD/../install
+    cmake -DCMAKE_INSTALL_PREFIX=$PWD/../install ../
     make
     make install
 popd

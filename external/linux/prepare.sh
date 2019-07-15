ef3711c4e43519ab598531976518a1c615a8ccab
Ian T. Jacobsen <iantj92@gmail.com>
2019-07-15 21:55:23 +0200

    SDL2 mixer added

--- external/linux/prepare.sh
+++ external/linux/prepare.sh
@@ -4,6 +4,7 @@
 ./libogg.sh
 ./libvorbis.sh
 ./SDL.sh
+./SDL_mixer.sh
 ./sndio.sh
 ./allegro.sh
 
@@ -12,6 +13,7 @@ PNG_DIR=$PWD/libpng-1.6.37
 OGG_DIR=$PWD/libogg-1.3.3
 VORBIS_DIR=$PWD/libvorbis-1.3.6
 SDL_DIR=$PWD/SDL2-2.0.9
+SDL_MIXER_DIR=$PWD/SDL2_mixer-2.0.4
 SNDIO_DIR=$PWD/sndio-1.5.0/libsndio
 ALLEGRO_DIR=$PWD/allegro5-4.4.3.1
 
@@ -30,6 +32,8 @@ pushd $PWD/prepared
     cp -r $VORBIS_DIR/install/include .
     cp $SDL_DIR/install/lib/libSDL2.a .
     cp -r $SDL_DIR/install/include .
+    cp $SDL_MIXER_DIR/install/lib/libSDL2_mixer.a .
+    cp -r $SDL_MIXER_DIR/install/include .
     cp $SNDIO_DIR/libsndio.a .
     cp -t $PWD/../../../libs $ALLEGRO_DIR/install/lib/*
 popd

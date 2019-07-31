2f9e9c35a324f91d361b6d09c30fd72bb2dadad7
Sebastian Riedel <git@basxto.de>
2019-07-31 12:58:15 +0200

    Apply boost patches to luabind

--- external/sources/luabind-0.9.1/luabind/object.hpp
+++ external/sources/luabind-0.9.1/luabind/object.hpp
@@ -536,6 +536,8 @@ namespace detail
       handle m_key;
   };
 
+#if BOOST_VERSION < 105700
+
 // Needed because of some strange ADL issues.
 
 #define LUABIND_OPERATOR_ADL_WKND(op) \
@@ -557,7 +559,9 @@ namespace detail
   LUABIND_OPERATOR_ADL_WKND(!=)
 
 #undef LUABIND_OPERATOR_ADL_WKND
- 
+
+#endif
+
 } // namespace detail
 
 namespace adl

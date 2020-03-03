08e63936e4c966e82a7e15f2e1412d1c232399dd
MaximDude <maximdude@gmail.com>
2020-03-03 23:07:52 +0200

    Cleanup in RTETools Remove some sneaky leftovers from LicenseMan removal and something that I don't even what it is, but it shouldn't exist

--- System/RTETools.h
+++ System/RTETools.h
@@ -1,216 +1,147 @@
-#ifndef _DDTTOOLS_
-#define _DDTTOOLS_
+#ifndef _RTETOOLS_
+#define _RTETOOLS_
 
-//////////////////////////////////////////////////////////////////////////////////////////
-// File:            DDTTools.h
-//////////////////////////////////////////////////////////////////////////////////////////
-// Description:     Header to include useful development tools and preprocessor tricks.
-// Project:         Retro Terrain Engine
-// Author(s):       Daniel Tabar
-//                  data@datarealms.com
-//                  http://www.datarealms.com
+// Header file for global utility methods.
 
 #include "RTEError.h"
 #include "Constants.h"
 
-struct TexMapTable;
-
 namespace RTE {
 
-class Vector;
+	class Vector;
 
+#pragma region Random Numbers
+	/// <summary>
+	/// Seeds the rand with the current runtime time.
+	/// </summary>
+	void SeedRand();
 
-//////////////////////////////////////////////////////////////////////////////////////////
-// Global function: SeedRand
-//////////////////////////////////////////////////////////////////////////////////////////
-// Description:     Seeds the rand with the current runtime time.
+	/// <summary>
+	/// A good rand function that return a float between 0.0 and 0.999.
+	/// </summary>
+	/// <returns>Random number.</returns>
+	double PosRand();
 
-void SeedRand();
+	/// <summary>
+	/// A good rand function that returns a floating point value between -1.0 and 1.0, both inclusive.
+	/// </summary>
+	/// <returns>Random number.</returns>
+	double NormalRand();
 
+	/// <summary>
+	/// A good rand function that returns a floating point value between two given thresholds, the min being inclusive, but the max not.
+	/// </summary>
+	/// <param name="min">Minimum value this can return.</param>
+	/// <param name="max">Maximum value this can return.</param>
+	/// <returns>Random number between limits.</returns>
+	double RangeRand(float min, float max);
 
-//////////////////////////////////////////////////////////////////////////////////////////
-// Global function: PosRand
-//////////////////////////////////////////////////////////////////////////////////////////
-// Description:     A good rand function that return a float between 0.0 and 0.999
+	/// <summary>
+	/// A rand function that returns an int between min and max, both inclusive.
+	/// </summary>
+	/// <param name="min">Minimum value this can return.</param>
+	/// <param name="max">Maximum value this can return.</param>
+	/// <returns>Random number between limits.</returns>
+	int SelectRand(int min, int max);
+#pragma endregion
 
-double PosRand();
+#pragma region Interpolation
+	/// <summary>
+	/// Simple Linear Interpolation, returns y for the last x passed in.
+	/// </summary>
+	/// <param name="xStart">X axis start value.</param>
+	/// <param name="xEnd">X axis end value.</param>
+	/// <param name="yStart">Y axis start value.</param>
+	/// <param name="yEnd">Y axis end value.</param>
+	/// <param name="progressScalar">Normalized positive progress scalar (0 - 1.0).</param>
+	/// <returns>Interpolated value.</returns>
+	float LERP(float xStart, float xEnd, float yStart, float yEnd, float progressScalar);
 
+	/// <summary>
+	/// Nonlinear ease-in interpolation. Starts slow.
+	/// </summary>
+	/// <param name="start">Start value.</param>
+	/// <param name="end">End value.</param>
+	/// <param name="progressScalar">Normalized positive progress scalar (0 - 1.0).</param>
+	/// <returns>Interpolated value.</returns>
+	float EaseIn(float start, float end, float progressScalar);
 
-//////////////////////////////////////////////////////////////////////////////////////////
-// Global function: NormalRand
-//////////////////////////////////////////////////////////////////////////////////////////
-// Description:     A good rand function that returns a floating point value between -1.0
-//                  and 1.0, both inclusive.
+	/// <summary>
+	/// Nonlinear ease-out interpolation. Slows down toward the end.
+	/// </summary>
+	/// <param name="start">Start value.</param>
+	/// <param name="end">End value.</param>
+	/// <param name="progressScalar">Normalized positive progress scalar (0 - 1.0).</param>
+	/// <returns>Interpolated value.</returns>
+	float EaseOut(float start, float end, float progressScalar);
 
-double NormalRand()/* { return (static_cast<double>(rand()) / (RAND_MAX / 2)) - 1.0; }*/;
+	/// <summary>
+	/// Nonlinear ease-in-out interpolation. Slows down in the start and end.
+	/// </summary>
+	/// <param name="start">Start value.</param>
+	/// <param name="end">End value.</param>
+	/// <param name="progressScalar">Normalized positive progress scalar (0 - 1.0).</param>
+	/// <returns>Interpolated value.</returns>
+	float EaseInOut(float start, float end, float progressScalar);
+#pragma endregion
 
+#pragma region Clamping
+	/// <summary>
+	/// Clamps a value between two limit values.
+	/// </summary>
+	/// <param name="value">Value to clamp.</param>
+	/// <param name="upperLimit">Upper limit of value.</param>
+	/// <param name="lowerLimit">Lower limit of value.</param>
+	/// <returns>True if either limit is currently reached, False if not.</returns>
+	bool Clamp(float &value, float upperLimit, float lowerLimit);
 
-//////////////////////////////////////////////////////////////////////////////////////////
-// Global function: RangeRand
-//////////////////////////////////////////////////////////////////////////////////////////
-// Description:     A good rand function that returns a floating point value between two
-//                  given thresholds, the min being inclusive, but the max not.
+	/// <summary>
+	/// Clamps a value between two limit values.
+	/// </summary>
+	/// <param name="value">Value to clamp.</param>
+	/// <param name="upperLimit">Upper limit of value.</param>
+	/// <param name="lowerLimit">Lower limit of value.</param>
+	/// <returns>Upper/Lower limit value if limit is currently reached, value between limits if not.</returns>
+	float Limit(float value, float upperLimit, float lowerLimit);
+#pragma endregion
 
-double RangeRand(float min, float max)/* { return min + ((max - min) * PosRand()); }*/;
+#pragma region Detection
+	/// <summary>
+	/// Tells whether a point is within a specified box.
+	/// </summary>
+	/// <param name="point">Vector position of the point we're checking.</param>
+	/// <param name="boxPos">Vector position of the box.</param>
+	/// <param name="width">Width of the box.</param>
+	/// <param name="height">Height of the box.</param>
+	/// <returns>True if point is inside box bounds.</returns>
+	bool WithinBox(Vector &point, Vector &boxPos, float width, float height);
 
+	/// <summary>
+	/// Tells whether a point is within a specified box.
+	/// </summary>
+	/// <param name="point">Vector position of the point we're checking.</param>
+	/// <param name="left">Position of box left plane (X start).</param>
+	/// <param name="top">Position of box top plane (Y start).</param>
+	/// <param name="right">Position of box right plane (X end).</param>
+	/// <param name="bottom">Position of box bottom plane (Y end).</param>
+	/// <returns>True if point is inside box bounds.</returns>
+	bool WithinBox(Vector &point, float left, float top, float right, float bottom);
+#pragma endregion
 
-//////////////////////////////////////////////////////////////////////////////////////////
-// Global function: SelectRand
-//////////////////////////////////////////////////////////////////////////////////////////
-// Description:     A rand function that returns an int between min and max, both inclusive.
+#pragma region Misc
+	/// <summary>
+	/// Fires up the default browser for the current OS on a specific URL.
+	/// </summary>
+	/// <param name="goToURL">A string with the URL to send the browser to.</param>
+	void OpenBrowserToURL(std::string goToURL);
 
-int SelectRand(int min, int max);
-
-
-//////////////////////////////////////////////////////////////////////////////////////////
-// Global function: LERP
-//////////////////////////////////////////////////////////////////////////////////////////
-// Description:     Simple Linear Interpolation, returns y for the last x passed in.
-
-float LERP(float xStart, float xEnd, float yStart, float yEnd, float xPos);
-
-
-//////////////////////////////////////////////////////////////////////////////////////////
-// Global function: EaseIn
-//////////////////////////////////////////////////////////////////////////////////////////
-// Description:     Nonlinear ease-in interpolation. Starts slow.
-// Arguments:       Start, end values, and the normalized positive progress scalar (0 - 1.0).
-
-float EaseIn(float x1, float x2, float y);
-
-
-//////////////////////////////////////////////////////////////////////////////////////////
-// Global function: EaseOut
-//////////////////////////////////////////////////////////////////////////////////////////
-// Description:     Nonlinear ease-out interpolation. Slows down toward the end.
-// Arguments:       Start, end values, and the normalized positive progress scalar (0 - 1.0).
-
-float EaseOut(float x1, float x2, float y);
-
-
-//////////////////////////////////////////////////////////////////////////////////////////
-// Global function: EaseInOut
-//////////////////////////////////////////////////////////////////////////////////////////
-// Description:     Nonlinear ease-in-out interpolation. Slows down in the start and end.
-// Arguments:       Start, end values, and the normalized positive progress scalar (0 - 1.0).
-
-float EaseInOut(float x1, float x2, float y);
-
-
-//////////////////////////////////////////////////////////////////////////////////////////
-// Global function: Clamp
-//////////////////////////////////////////////////////////////////////////////////////////
-// Description:     Clamps a value between two limit values.
-// Arguments:       Value to clamp, the upper and the lower limit values.
-// Return value:    Whether the value was clamped or not.
-
-bool Clamp(float &value, float upperLimit, float lowerLimit);
-
-
-//////////////////////////////////////////////////////////////////////////////////////////
-// Global function: Limit
-//////////////////////////////////////////////////////////////////////////////////////////
-// Description:     Clamps a value between two limit values.
-// Arguments:       Value to clamp, the upper and the lower limit values.
-// Return value:    The clamped value.
-
-float Limit(float value, float upperLimit, float lowerLimit);
-
-
-//////////////////////////////////////////////////////////////////////////////////////////
-// Global function: WithinBox
-//////////////////////////////////////////////////////////////////////////////////////////
-// Description:     Tells whether a point is within a specified box.
-
-bool WithinBox(Vector &point, Vector &topLeft, float width, float height);
-
-
-//////////////////////////////////////////////////////////////////////////////////////////
-// Global function: WithinBox
-//////////////////////////////////////////////////////////////////////////////////////////
-// Description:     Tells whether a point is within a specified box.
-
-bool WithinBox(Vector &point, float left, float top, float right, float bottom);
-
-
-//////////////////////////////////////////////////////////////////////////////////////////
-// Global function: XORStrings
-//////////////////////////////////////////////////////////////////////////////////////////
-// Description:     Encrypts/decrypts one string with another with XOR encryption.
-//                  Returns the result. Inputs don't have to be the same length.
-//                  Won't encrypt any chars in the toEncrypt string that match the exception.
-
-std::string XORStrings(std::string toEncrypt, std::string keyString, char exception = ' ');
-
-
-//////////////////////////////////////////////////////////////////////////////////////////
-// Global function: OpenBrowserToURL
-//////////////////////////////////////////////////////////////////////////////////////////
-// Description:     Fires up the default browser for the current OS on a specific URL.
-// Arguments:       A string with the URL to send the browser to.
-// Return value:    None.
-
-void OpenBrowserToURL(std::string goToURL);
-
-
-//////////////////////////////////////////////////////////////////////////////////////////
-// Global function: XTEAEncrypt
-//////////////////////////////////////////////////////////////////////////////////////////
-// Description:     Encrypts one string with a key with XTEA encryption.
-//                  Returns the result.
-//                  http://en.wikipedia.org/wiki/XTEA#Implementations
-
-std::string XTEAEncrypt(std::string toEncrypt, std::string keyString, int numRounds = 32);
-
-
-//////////////////////////////////////////////////////////////////////////////////////////
-// Global function: XTEADecrypt
-//////////////////////////////////////////////////////////////////////////////////////////
-// Description:     Decrypts one string with a key with XTEA encryption.
-//                  Returns the result.
-//                  http://en.wikipedia.org/wiki/XTEA#Implementations
-
-std::string XTEADecrypt(std::string encrypted, std::string keyString, int numRounds = 32);
-
-
-//////////////////////////////////////////////////////////////////////////////////////////
-// Global function: ASCIIFileContainsString
-//////////////////////////////////////////////////////////////////////////////////////////
-// Description:     Searches through an ascii file on disk for a specific string and tells
-//                  whether it was found or not.
-// Arguments:       The path to the ascii file to search.
-//                  The exact string to look for; case sensitive.
-// Return value:    Whether the file was found, AND that string was found in that that file.
-
-bool ASCIIFileContainsString(std::string filePath, std::string findString);
-
-/*
-//////////////////////////////////////////////////////////////////////////////////////////
-// Global function: DrawMaterial
-//////////////////////////////////////////////////////////////////////////////////////////
-// Description:     Draws a regular (non rotozoomed)BITMAP's silhuette as a solid
-//                  color/material index to a BITMAP of choice.
-// Arguments:       A pointer to the BITMAP whose silhuette will be drawn.
-//                  A pointer to a BITMAP to draw on.
-//                  A color/material index that the silhuette will be filled with.
-// Return value:    None.
-
-void DrawMaterial(BITMAP *pSprite, BITMAP *pTargetBitmap, unsigned char fillByte);
-
-
-//////////////////////////////////////////////////////////////////////////////////////////
-// Global function: DrawMaterialRotoZoomed
-//////////////////////////////////////////////////////////////////////////////////////////
-// Description:     Draws a rotated BITMAP's silhuette as a solid color/material index
-//                  to a BITMAP of choice.
-// Arguments:       A pointer to the BITMAP whose silhuette will be drawn.
-//                  A pointer to a BITMAP to draw on.
-//                  A color/material index that the silhuette will be filled with.
-// Return value:    None.
-
-void DrawMaterialRotoZoomed(BITMAP *pSprite, BITMAP *pTargetBitmap, unsigned char fillByte);
-*/
-
-} // Namespace RTE
-
-#endif // File
+	/// <summary>
+	/// Searches through an ASCII file on disk for a specific string and tells whether it was found or not.
+	/// </summary>
+	/// <param name="">The path to the ASCII file to search.</param>
+	/// <param name="">The exact string to look for. Case sensitive!</param>
+	/// <returns>Whether the file was found AND that string was found in that file.</returns>
+	bool ASCIIFileContainsString(std::string filePath, std::string findString);
+#pragma endregion
+}
+#endif

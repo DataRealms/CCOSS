23e13829c573d0a802a1ea9be171e6c5dd59613a
MaximDude <maximdude@gmail.com>
2020-03-06 21:46:50 +0200

    Cleanup in System Move LogToCLI from Main to System

--- Main.cpp
+++ Main.cpp
@@ -194,38 +194,6 @@ END_OF_FUNCTION(QuitHandler)
 
 void LoadingSplashProgressReport(std::string reportString, bool newItem = false)
 {
-	if(g_LogToCli)
-	{
-		if (newItem)
-		{
-			std::cout << std::endl;
-		}
-		// Overwrite current line
-		std::cout << "\r";
-		size_t startPos = 0;
-		// Just make sure to really overwrite all old output
-		// " - done! ✓" is shorter than "reading line 700"
-		std::string unicoded = reportString + "          ";
-		// Colorize output with ANSI escape code
-		std::string greenTick = "\033[1;32m✓\033[0;0m";
-		// Convert all ✓ characters to unicode
-		// It's the 42th from last character in CC's custom font
-		while ((startPos = unicoded.find(-42, startPos)) != std::string::npos)
-		{
-			unicoded.replace(startPos, 1, greenTick);
-			// We don't have to check indices we just overwrote
-			startPos += greenTick.length();
-		}
-		startPos = 0;
-		std::string yellowDot = "\033[1;33m•\033[0;0m";
-		// Convert all • characters to unicode
-		while ((startPos = unicoded.find(-43, startPos)) != std::string::npos)
-		{
-			unicoded.replace(startPos, 1, yellowDot);
-			startPos += yellowDot.length();
-		}
-		std::cout << unicoded << std::flush;
-	}
 	if (g_pLoadingGUI)
 	{
 		g_UInputMan.Update();
--- System/System.cpp
+++ System/System.cpp
@@ -1,36 +1,53 @@
-#include "System.h"
+﻿#include "System.h"
+#include <direct.h>
 
-    #include <direct.h>
-    #define getcwd _getcwd
+#define getcwd _getcwd
 
-namespace RTE
-{
+namespace RTE {
 
-System g_System;
+	System g_System;
 
-void System::ChangeWorkingDirectory()
-{
-}
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-//////////////////////////////////////////////////////////////////////////////////////////
-// Method:  GetWorkingDirectory
-//////////////////////////////////////////////////////////////////////////////////////////
-// Description:     Returns current working directory.
-std::string System::GetWorkingDirectory()
-{
-	char cwd[1024];
-	if (getcwd(cwd, sizeof(cwd)))
-		return std::string(cwd);
-	return std::string(".");
-}
-	
-//////////////////////////////////////////////////////////////////////////////////////////
-// Method:  MakeDirectory
-//////////////////////////////////////////////////////////////////////////////////////////
-// Description:     Create a directory on disk, return error code if any
-int System::MakeDirectory(const std::string& path)
-{
-	return _mkdir(path.c_str());
-}
-	
+	std::string System::GetWorkingDirectory() {
+		char cwd[1024];
+		if (getcwd(cwd, sizeof(cwd))) {
+			return std::string(cwd);
+		}
+		return std::string(".");
+	}
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+	int System::MakeDirectory(const std::string& path) { return _mkdir(path.c_str()); }
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+	void System::LogToCLI(std::string reportString, bool newItem) {
+		if (newItem) { std::cout << std::endl; }
+		// Overwrite current line
+		std::cout << "\r";
+		size_t startPos = 0;
+		// Just make sure to really overwrite all old output
+		// " - done! ✓" is shorter than "reading line 700"
+		std::string unicoded = reportString + "          ";
+		// Colorize output with ANSI escape code
+		std::string greenTick = "\033[1;32m✓\033[0;0m";
+
+		// Convert all ✓ characters to unicode
+		// It's the 42th from last character in CC's custom font
+		while ((startPos = unicoded.find(-42, startPos)) != std::string::npos) {
+			unicoded.replace(startPos, 1, greenTick);
+			// We don't have to check indices we just overwrote
+			startPos += greenTick.length();
+		}
+		startPos = 0;
+		std::string yellowDot = "\033[1;33m•\033[0;0m";
+		// Convert all • characters to unicode
+		while ((startPos = unicoded.find(-43, startPos)) != std::string::npos) {
+			unicoded.replace(startPos, 1, yellowDot);
+			startPos += yellowDot.length();
+		}
+		std::cout << unicoded << std::flush;
+	}
 }

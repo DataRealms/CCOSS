3149d6099ddc2f1b902690813f6610b6c08b765c
Gareth YR <garethrickwood@hotmail.com>
2020-04-19 03:01:45 -0300

    Added a bunch of features to SoundContainer - the ability to add groups and add sounds to specific groups (that way you can play compound sounds), the ability to set offsets and minimum audible distance and override attenuation start distance for individual sounds in sound groups, the ability to set cycle modes to choose how SelectNextSoundSet acts Cleaned up various stuff in SoundContainer

--- Entities/SoundContainer.h
+++ Entities/SoundContainer.h
@@ -16,6 +16,20 @@ namespace RTE {
 	public:
 		ENTITYALLOCATION(SoundContainer)
 		CLASSINFOGETTERS
+		
+		enum SoundCycleMode {
+			MODE_RANDOM = 0,
+			MODE_FORWARDS = 1
+		};
+
+		struct SoundData {
+			ContentFile SoundFile;
+			FMOD::Sound *SoundObject;
+			Vector Offset = Vector();
+			float MinimumAudibleDistance = 0;
+			float AttenuationStartDistance = c_DefaultAttenuationStartDistance;
+			FMOD_VECTOR CustomRolloffPoints[2];
+		};
 
 #pragma region Creation
 		/// <summary>
@@ -44,18 +58,18 @@ namespace RTE {
 		/// <param name="attenuationStartDistance">The distance at which this SoundContainer's sounds should start attenuating away.</param>
 		/// <param name="immobile">Whether this SoundContainer's sounds will be treated as immobile, i.e. they won't be affected by 3D sound manipulation.</param>
 		/// <returns>An error return value signaling success or any particular failure. Anything below 0 is an error signal.</returns>
-		int Create(int loops = 0, bool affectedByGlobalPitch = true, float attenuationStartDistance = 1, bool immobile = false) { SetLoopSetting(loops); m_AffectedByGlobalPitch = affectedByGlobalPitch; m_AttenuationStartDistance = attenuationStartDistance; m_Immobile = immobile; return 0; }
+		int Create(int loops = 0, bool affectedByGlobalPitch = true, float attenuationStartDistance = -1, bool immobile = false) { SetLoopSetting(loops); m_AffectedByGlobalPitch = affectedByGlobalPitch; SetAttenuationStartDistance(attenuationStartDistance); m_Immobile = immobile; return 0; }
 
 		/// <summary>
 		/// Creates a SoundContainer and gives it a path to its first sound.
 		/// </summary>
-		/// <param name="soundPath">A path to the sound for this sound to have.</param>
+		/// <param name="soundFilePath">A path to a sound to add to the first SoundSet of this SoundContainer.</param>
 		/// <param name="loops">The number of times this SoundContainer's sounds will loop. 0 means play once. -1 means play infinitely until stopped.</param>
 		/// <param name="affectedByGlobalPitch">Whether this SoundContainer's sounds' frequency will be affected by the global pitch.</param>
 		/// <param name="attenuationStartDistance">The distance at which this SoundContainer's sounds should start attenuating away.</param>
 		/// <param name="immobile">Whether this SoundContainer's sounds will be treated as immobile, i.e. they won't be affected by 3D sound manipulation.</param>
 		/// <returns>An error return value signaling success or any particular failure. Anything below 0 is an error signal.</returns>
-		int Create(std::string const soundPath, int loops = 0, bool affectedByGlobalPitch = true, float attenuationStartDistance = 1, bool immobile = false) { int result = Create(loops, affectedByGlobalPitch, attenuationStartDistance, immobile); AddSound(soundPath); return result; }
+		int Create(std::string const soundFilePath, int loops = 0, bool affectedByGlobalPitch = true, float attenuationStartDistance = -1, bool immobile = false) { int result = Create(loops, affectedByGlobalPitch, attenuationStartDistance, immobile); AddSound(soundFilePath); return result; }
 
 		/// <summary>
 		/// Adds a new sound to this SoundContainer, spitting out a lua error if it fails.
@@ -80,7 +94,7 @@ namespace RTE {
 		/// <param name="offset">The offset position to play this sound at, where (0, 0) is no offset.</param>
 		/// <param name="attenuationStartDistance">The attenuation start distance for this sound, -1 is default.</param>
 		/// <param name="abortGameForInvalidSound">Whether to abort the game if the sound couldn't be added, or just show a console error.</param>
-		void AddSound(std::string const &soundFilePath, const Vector &offset, float attenuationStartDistance, bool abortGameForInvalidSound) { return AddSound(soundFilePath, m_SoundSets.size() + 1, Vector(), c_DefaultAttenuationStartDistance, abortGameForInvalidSound); }
+		void AddSound(std::string const &soundFilePath, const Vector &offset, float attenuationStartDistance, bool abortGameForInvalidSound) { return AddSound(soundFilePath, m_SoundSets.size() + 1, Vector(), 0, c_DefaultAttenuationStartDistance, abortGameForInvalidSound); }
 
 		/// <summary>
 		/// Adds a new sound to this SoundContainer, either spitting out a lua error or aborting if it fails.
@@ -89,9 +103,10 @@ namespace RTE {
 		/// <param name="soundFilePath">A path to the new sound to add. This will be handled through PresetMan.</param>
 		/// <param name="soundSetIndex">The SoundSet index to add this new Sound to. If it's not an existing index, a new SoundSet will be added with this Sound.</param>
 		/// <param name="offset">The offset position to play this sound at, where (0, 0) is no offset.</param>
-		/// <param name="attenuationStartDistance">The attenuation start distance for this sound, -1 is default.</param>
+		/// <param name="minimumAudibleDistance">The minimum distance at which this sound will be audible. 0 means there is none, which is normally the case.</param>
+		/// <param name="attenuationStartDistance">The attenuation start distance for this sound, -1 sets it to default.</param>
 		/// <param name="abortGameForInvalidSound">Whether to abort the game if the sound couldn't be added, or just show a console error.</param>
-		void AddSound(std::string const &soundFilePath, unsigned int soundSetIndex, const Vector &offset, float attenuationStartDistance, bool abortGameForInvalidSound);
+		void AddSound(std::string const &soundFilePath, unsigned int soundSetIndex, const Vector &offset, float minimumAudibleDistance, float attenuationStartDistance, bool abortGameForInvalidSound);
 #pragma endregion
 
 #pragma region Destruction
@@ -127,6 +142,21 @@ namespace RTE {
 		virtual int ReadProperty(std::string propName, Reader &reader);
 
 		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="propName"></param>
+		/// <param name="reader"></param>
+		/// <returns></returns>
+		int ReadSoundSet(std::string propName, Reader &reader);
+
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="reader"></param>
+		/// <returns></returns>
+		SoundData ReadSound(Reader &reader);
+
+		/// <summary>
 		/// Saves the complete state of this SoundContainer to an output stream for later recreation with Create(Reader &reader).
 		/// </summary>
 		/// <param name="writer">A Writer that the SoundContainer will save itself with.</param>
@@ -139,49 +169,25 @@ namespace RTE {
 		/// Gets the current list of sounds in the SoundContainer.
 		/// </summary>
 		/// <returns>A reference to the list.</returns>
-		const std::vector<std::pair<ContentFile, FMOD::Sound *>> *GetSounds() const { return &m_Sounds; }
+		const std::vector<std::vector<SoundData>> *GetSounds() const { return &m_SoundSets; }
 
 		/// <summary>
 		/// Shows whether this SoundContainer has been initialized at all yet and loaded with any samples.
 		/// </summary>
 		/// <returns>Whether this sound has any samples.</returns>
-		bool HasAnySounds() const { return GetSoundCount() > 0; }
-
-		/// <summary>
-		/// Gets the current number of sounds in this SoundContainer.
-		/// </summary>
-		/// <returns>An int with the count.</returns>
-		int GetSoundCount() const { return m_Sounds.size(); }
-
-		/// <summary>
-		/// Indicates whether any sound in this SoundContainer is currently being played.
-		/// </summary>
-		/// <returns>Whether any sounds are playing.</returns>
-		bool IsBeingPlayed() { return GetPlayingSoundCount() > 0; }
-
-		/// <summary>
-		/// Gets the number of currently playing sounds in this SoundContainer.
-		/// </summary>
-		/// <returns>Number of playing sounds in this SoundContainer.</returns>
-		int GetPlayingSoundCount() { return m_PlayingChannels.size(); }
-
-		/// <summary>
-		/// Gets a vector of hashes of the sounds selected to be played next in this SoundContainer.
-		/// </summary>
-		/// <returns>The currently playing sounds hashes.</returns>
-		std::vector<size_t> GetSelectedSoundHashes();
-
-		/// <summary>
-		/// Gets a vector of the sounds objects selected to be played next in this SoundContainer.
-		/// </summary>
-		/// <returns>The currently playing sound objects.</returns>
-		std::vector<FMOD::Sound *> GetSelectedSoundObjects();
+		bool HasAnySounds() const { return m_SoundSets.size() > 0; }
 
 		/// <summary>
 		/// Gets the channels playing sounds from this SoundContainer.
 		/// </summary>
 		/// <returns>The channels currently being used.</returns>
 		std::unordered_set<unsigned short> *GetPlayingChannels() { return &m_PlayingChannels; }
+
+		/// <summary>
+		/// Indicates whether any sound in this SoundContainer is currently being played.
+		/// </summary>
+		/// <returns>Whether any sounds are playing.</returns>
+		bool IsBeingPlayed() { return m_PlayingChannels.size() > 0; }
 
 		/// <summary>
 		/// Adds a channel index to the SoundContainer's collection of playing channels.
@@ -194,6 +200,37 @@ namespace RTE {
 		/// </summary>
 		/// <param name="channel">The channel index to remove.</param>
 		void RemovePlayingChannel(unsigned short channel) { m_PlayingChannels.erase(channel); }
+
+		/// <summary>
+		/// Gets the current sound selection cycle mode, which is used to determine what SoundSet to select next time SelectNextSoundSet is called.
+		/// </summary>
+		/// <returns>The current sound selection cycle mode.</returns>
+		SoundCycleMode GetSoundSelectionCycleMode() { return m_SoundSelectionCycleMode; }
+
+		/// <summary>
+		/// Sets the current sound selection cycle mode, which is used to determine what SoundSet to select next time SelectNextSoundSet is called.
+		/// </summary>
+		/// <param name="soundSelectionCycleMode">The sound selection cycle mode to use.</param>
+		void SetSoundSelectionCycleMode(SoundCycleMode soundSelectionCycleMode) { m_SoundSelectionCycleMode = soundSelectionCycleMode; }
+
+		/// <summary>
+		/// Gets the selected SoundSet for this SoundContainer. The selected SoundSet is changed with SelectNextSoundSet.
+		/// </summary>
+		/// <returns>The selected SoundSet.</returns>
+		std::vector<SoundData> GetSelectedSoundSet() { return m_SoundSets[m_SelectedSoundSet]; }
+
+		/// <summary>
+		/// Gets a vector of hashes of the sounds selected to be played next in this SoundContainer.
+		/// </summary>
+		/// <returns>The currently playing sounds hashes.</returns>
+		std::vector<size_t> GetSelectedSoundHashes();
+
+		/// <summary>
+		/// Gets the SoundData object that corresponds to the given FMOD::Sound. If the sound can't be found, it returns a null pointer.
+		/// </summary>
+		/// <param name="sound">The FMOD::Sound to search for.</param>
+		/// <returns>A pointer to the corresponding SoundData or a null pointer.</returns>
+		SoundData *GetSoundDataForSound(FMOD::Sound *sound);
 #pragma endregion
 
 #pragma region Sound Property Getters and Setters
@@ -211,10 +248,10 @@ namespace RTE {
 		float GetAttenuationStartDistance() const { return m_AttenuationStartDistance; }
 
 		/// <summary>
-		/// Sets the attenuation start distance of this SoundContainer. 
+		/// Sets the attenuation start distance of this SoundContainer. Values < 0 set it to default. Does not affect currently playing sounds.
 		/// </summary>
 		/// <param name="attenuationStartDistance">The new attenuation start distance.</param>
-		void SetAttenuationStartDistance(float attenuationStartDistance) { m_AttenuationStartDistance = attenuationStartDistance; m_AllSoundPropertiesUpToDate = false; }
+		void SetAttenuationStartDistance(float attenuationStartDistance) { m_AttenuationStartDistance = attenuationStartDistance < 0 ? c_DefaultAttenuationStartDistance : attenuationStartDistance; m_AllSoundPropertiesUpToDate = false; }
 
 		/// <summary>
 		/// Gets the looping setting of this SoundContainer.
@@ -223,7 +260,7 @@ namespace RTE {
 		int GetLoopSetting() const { return m_Loops; }
 
 		/// <summary>
-		/// Sets the looping setting of this SoundContainer. 
+		/// Sets the looping setting of this SoundContainer. Does not affect currently playing sounds.
 		/// 0 means the sound is set to only play once. -1 means it loops indefinitely.
 		/// </summary>
 		/// <param name="loops">The new loop count.</param>
@@ -236,7 +273,7 @@ namespace RTE {
 		int GetPriority() const { return m_Priority; }
 
 		/// <summary>
-		/// Sets the current playback priority. Higher value will make this more likely to make it into mixing on playback.
+		/// Sets the current playback priority. Higher priority (lower value) will make this more likely to make it into mixing on playback. Does not affect currently playing sounds.
 		/// </summary>
 		/// <param name="priority">The new priority. See AudioMan::PRIORITY_* enumeration.</param>
 		void SetPriority(int priority) { m_Priority = Limit(priority, 255, 0); }
@@ -248,7 +285,7 @@ namespace RTE {
 		bool IsAffectedByGlobalPitch() const { return m_AffectedByGlobalPitch; }
 
 		/// <summary>
-		/// Sets whether the sounds in this SoundContainer are affected by global pitch changes or not.
+		/// Sets whether the sounds in this SoundContainer are affected by global pitch changes or not. Does not affect currently playing sounds.
 		/// </summary>
 		/// <param name="pitched">The new affected by global pitch setting.</param>
 		void SetAffectedByGlobalPitch(bool affectedByGlobalPitch) { m_AffectedByGlobalPitch = affectedByGlobalPitch; }
@@ -260,7 +297,7 @@ namespace RTE {
 		bool IsImmobile() const { return m_Immobile; }
 
 		/// <summary>
-		/// Sets whether the sounds in this SoundContainer should be considered immobile, i.e. always play at the listener's position.
+		/// Sets whether the sounds in this SoundContainer should be considered immobile, i.e. always play at the listener's position. Does not affect currently playing sounds.
 		/// </summary>
 		/// <param name="immobile">The new immobile setting.</param>
 		void SetImmobile(bool immobile) { m_Immobile = immobile; m_AllSoundPropertiesUpToDate = false; }
@@ -317,7 +354,7 @@ namespace RTE {
 		/// <summary>
 		/// Selects the next sounds of this SoundContainer to be played.
 		/// </summary>
-		bool SelectNextSounds();
+		bool SelectNextSoundSet();
 
 		/// <summary>
 		/// Fades out playback of the SoundContainer to 0 volume.
@@ -338,9 +375,11 @@ namespace RTE {
 	protected:
 
 		static Entity::ClassInfo m_sClass; //!< ClassInfo for this class.
+		static const std::unordered_map<std::string, SoundCycleMode> c_CycleModeMap; //!< A map of to support string parsing for the CycleMode enum. Populated in the implementing cpp file.
 
-		std::vector<std::pair<ContentFile, FMOD::Sound *>> m_Sounds; // All the FMOD Sound objects in this SoundContainer, paired with their appropriate ContentFile. The sound objects within are owned by the ContentFile static maps
-		std::vector<size_t> m_SelectedSounds; //!< Vector of the indices of all selected sounds
+		std::vector<std::vector<SoundData>> m_SoundSets; //The vector of SoundSets in this SoundContainer, wherein a SoundSet is a vector containing one or more SoundData structs.
+		size_t m_SelectedSoundSet; //!< The selected SoundSet for this SoundContainer, used to determine what sounds will play when Play is called.
+		SoundCycleMode m_SoundSelectionCycleMode; //!< The sound cycle mode for this sound container, used to determine what will play next, each time play is called.
 
 		std::unordered_set<unsigned short> m_PlayingChannels; //!< The channels this SoundContainer is currently using
 
@@ -357,6 +396,14 @@ namespace RTE {
 		/// Clears all the member variables of this SoundContainer, effectively resetting the members of this abstraction level only.
 		/// </summary>
 		void Clear();
+
+		/// <summary>
+		/// TODO This is currently not used in favour of a simpler 2-point method but is kept in case it should be changed back. Examine this and remove or use it in future.
+		/// </summary>
+		/// <param name="soundDataToCalculateFor"></param>
+		/// <param name="rolloffPoints"></param>
+		/// <param name="numRolloffPoints"></param>
+		void CalculateCustomRolloffPoints(const SoundData &soundDataToCalculateFor, FMOD_VECTOR *rolloffPoints, int numRolloffPoints);
 	};
 }
 #endif

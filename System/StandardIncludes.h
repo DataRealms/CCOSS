1ce4786578d934ada45a6271aed407af7541f92a
Leo Goettlicher <lesgoets@gmail.com>
2020-08-28 03:59:19 +0200

    sorted StandardIncludes.h

--- System/StandardIncludes.h
+++ System/StandardIncludes.h
@@ -2,51 +2,51 @@
 #define _RTEINCLUDES_
 
 // Inclusion of relevant C++ Standard Library headers.
-#include <cstdlib>
+#include <algorithm>
+#include <array>
+#include <cassert>
+#include <cctype>
+#include <cerrno>
 #include <cstdarg>
 #include <cstddef>
-#include <functional>
-#include <thread>
-#include <mutex>
-#include <cctype>
-#include <string>
+#include <cstdlib>
 #include <cstring>
-#include <vector>
+#include <ctime>
+#include <deque>
+#include <fstream>
+#include <functional>
+#include <iostream>
+#include <istream>
+#include <limits>
 #include <list>
-#include <set>
 #include <map>
+#include <mutex>
+#include <queue>
+#include <random>
+#include <regex>
+#include <set>
+#include <sstream>
+#include <string>
+#include <thread>
 #include <unordered_map>
 #include <unordered_set>
-#include <queue>
-#include <deque>
-#include <regex>
-#include <iostream>
-#include <sstream>
-#include <fstream>
-#include <istream>
-#include <algorithm>
-#include <cmath>
-#include <ctime>
-#include <cerrno>
-#include <cassert>
-#include <limits>
-#include <random>
-#include <array>
-// TODO: don't forget to replace with <filesystem> when eventually enable ++17 standard.
+#include <vector>
+// TODO: don't forget to replace with <filesystem> when eventually enable ++17
+// standard.
 #include <experimental/filesystem>
 
 // TODO: Get rid of these once alias qualifiers are added.
-using std::string;
-using std::list;
-using std::pair;
-using std::deque;
-using std::map;
-using std::set;
-using std::vector;
-using std::ios_base;
 using std::array;
+using std::deque;
+using std::ios_base;
+using std::list;
 using std::make_pair;
-using std::min;
+using std::map;
 using std::max;
+using std::min;
+using std::pair;
+using std::set;
+using std::string;
+using std::vector;
 
 #endif

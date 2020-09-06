ae38540dd7281f6d70b5efb099238e0059f01c59
Leo Goettlicher <lesgoets@gmail.com>
2020-09-06 19:36:00 +0200

    Revert "replaced duplicate RTEAssert and RTEError by cleaner solution"
    This reverts commit 8e3ab6e6c41cce77fedb890743819fb15fc349b1.

--- System/RTEError.cpp
+++ System/RTEError.cpp
@@ -5,65 +5,61 @@ namespace RTE {
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-extern void ShowMessageBox(std::string message) { allegro_message(message.c_str()); }
+	extern void ShowMessageBox(std::string message) { allegro_message(message.c_str()); }
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-extern bool RTEAbortFunc(const char *description, const char *file, int line) {
-	// Save out the screen bitmap, after making a copy of it, faster sometimes
-	if (screen) {
-		if (!g_ScreendumpBuffer) {
-			g_ScreendumpBuffer= create_bitmap(screen->w, screen->h);
+	extern bool RTEAbortFunc(const char *description, const char *file, int line) {
+		// Save out the screen bitmap, after making a copy of it, faster sometimes
+		if (screen) {
+			if (!g_ScreendumpBuffer) { g_ScreendumpBuffer = create_bitmap(screen->w, screen->h); }
+			blit(screen, g_ScreendumpBuffer, 0, 0, 0, 0, screen->w, screen->h);
+			PALETTE palette;
+			get_palette(palette);
+			save_bmp("abortscreen.bmp", g_ScreendumpBuffer, palette);
 		}
-		blit(screen, g_ScreendumpBuffer, 0, 0, 0, 0, screen->w, screen->h);
-		PALETTE palette;
-		get_palette(palette);
-		save_bmp("abortscreen.bmp", g_ScreendumpBuffer, palette);
-	}
-	// Ditch the video mode so the message box appears without problems
-	if (screen != 0) {
-		set_gfx_mode(GFX_TEXT, 0, 0, 0, 0);
-	}
-	// Set title of the messagebox
-	set_window_title("RTE Aborted! (x_x)");
+		// Ditch the video mode so the message box appears without problems
+		if (screen != 0) { set_gfx_mode(GFX_TEXT, 0, 0, 0, 0); }
+		// Set title of the messagebox
+		set_window_title("RTE Aborted! (x_x)");
 
-	char message[512];
+		char message[512];
 
-#if defined DEBUG_BUILD || defined MIN_DEBUG_BUILD
-	// Show message box with explanation
-	sprintf_s(message, sizeof(message), "Runtime Error in file %s, line %i, because:\n\n%s\n\nThe last frame has been dumped to 'abortscreen.bmp'", file, line, description);
-	allegro_message(message);
+#if defined DEBUG_BUILD || defined MIN_DEBUG_BUILD 	
+		// Show message box with explanation
+		sprintf_s(message, sizeof(message), "Runtime Error in file %s, line %i, because:\n\n%s\n\nThe last frame has been dumped to 'abortscreen.bmp'", file, line, description);
+		allegro_message(message);
 #else
-	// Shortened and less confusing one. users have no use of knowing which source file and where.
-	sprintf_s(message, sizeof(message), "%s\n\nThe last frame has been dumped to 'abortscreen.bmp'", description);
-	allegro_message(message);
+		// Shortened and less confusing one. users have no use of knowing which source file and where.
+		sprintf_s(message, sizeof(message), "%s\n\nThe last frame has been dumped to 'abortscreen.bmp'", description);
+		allegro_message(message);
 #endif
-	// True so that the debugbreak code is run and the debugger goes there.
-	return true;
-}
-
-/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-extern bool RTEAbortFunc(const std::string description, const char *file, int line) {
-	return RTEAbortFunc(description.c_str(), file, line);
-}
-
-/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-bool RTEAssertFunc(bool expression, const char *description, const char *file, int line, bool &alwaysIgnore) {
-	if (!expression) {
-		// TODO: Make this display a box in the game asking whether to ignore or abort. For now, always abort.
-		RTEAbortFunc(description, __FILE__, __LINE__);
-
 		// True so that the debugbreak code is run and the debugger goes there.
 		return true;
 	}
-	return false;
-}
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-extern bool RTEAssertFunc(bool expression, std::string description, const char *file, int line, bool &alwaysIgnore) {
-	return RTEAssertFunc(expression, description.c_str(), file, line, alwaysIgnore);
+	extern bool RTEAbortFunc(const std::string description, const char *file, int line) {
+		return RTEAbortFunc(description.c_str(), file, line);
+	}
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+	bool RTEAssertFunc(bool expression, const char *description, const char *file, int line, bool &alwaysIgnore) {
+		if (!expression) {
+			// TODO: Make this display a box in the game asking whether to ignore or abort. For now, always abort.
+			RTEAbortFunc(description, __FILE__, __LINE__);
+			
+			// True so that the debugbreak code is run and the debugger goes there.
+			return true;
+		}
+		return false;
+	}
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+	extern bool RTEAssertFunc(bool expression, std::string description, const char *file, int line, bool &alwaysIgnore) {
+		return RTEAssertFunc(expression, description.c_str(), file, line, alwaysIgnore);
+	}
 }
-} // namespace RTE
--- System/RTEError.h
+++ System/RTEError.h
@@ -4,56 +4,63 @@
 #include "allegro.h"
 #include "loadpng.h"
 
-#ifdef _WIN32
-#define DEBUGGERBREAK __debugbreak();
-#else
-#define DEBUGGERBREAK ;
-#endif
-
 namespace RTE {
 
-static BITMAP *g_ScreendumpBuffer= 0; //!< Buffer for saving abort screendumps.
+	static BITMAP *g_ScreendumpBuffer = 0; //!< Buffer for saving abort screendumps.
 
-/// <summary>
-/// Pops up a message box dialog in the OS. For debug purposes mostly.
-/// </summary>
-/// <param name="message">The string that the message box should display.</param>
-extern void ShowMessageBox(std::string message);
+	/// <summary>
+	/// Pops up a message box dialog in the OS. For debug purposes mostly.
+	/// </summary>
+	/// <param name="message">The string that the message box should display.</param>
+	extern void ShowMessageBox(std::string message);
 
-/// <summary>
-/// Abort on Error function. Will try to dump a screenshot, show an abort message, and then quit the program immediately.
-/// </summary>
-/// <param name="description">The description of the abortion.</param>
-/// <param name="file">The source file in which the abortion is made.</param>
-/// <param name="line">The line where the abortion is made.</param>
-extern bool RTEAbortFunc(const char *description, const char *file, int line);
-extern bool RTEAbortFunc(const std::string description, const char *file, int line);
-#define RTEAbort(description)                            \
-	{                                                      \
-		if (RTEAbortFunc(description, __FILE__, __LINE__)) { \
-			DEBUGGERBREAK                                      \
-		}                                                    \
-	}
-/// <summary>
-/// A souped-up, customized assert function that brings up a nice dialog box on assert failure.
-/// The user can choose to break or ignore the particular assertion failure once, or to always ignore.
-/// </summary>
-/// <param name="expression">The expression that will be asserted to be true.</param>
-/// <param name="description">The description of the assertion.</param>
-/// <param name="file">The source file in which the assertion is made.</param>
-/// <param name="line">The line where the assertion is made.</param>
-/// <param name="alwaysIgnore">A reference to a bool that is used in an "Always ignore" functionality.</param>
-/// <returns>Whether the assertion failed AND the user chose to break in the dialog box.</returns>
-extern bool RTEAssertFunc(bool expression, const char *description, const char *file, int line, bool &alwaysIgnore);
-extern bool RTEAssertFunc(bool expression, const std::string description, const char *file, int line, bool &alwaysIgnore);
-#define RTEAssert(expression, description)                                            \
-	{                                                                                   \
-		static bool alwaysIgnore= false;                                                  \
-		if (!alwaysIgnore) {                                                              \
-			if (RTEAssertFunc(expression, description, __FILE__, __LINE__, alwaysIgnore)) { \
-				DEBUGGERBREAK                                                                 \
-			}                                                                               \
-		}                                                                                 \
-	}
-} // namespace RTE
+	/// <summary>
+	/// Abort on Error function. Will try to dump a screenshot, show an abort message, and then quit the program immediately.
+	/// </summary>
+	/// <param name="description">The description of the abortion.</param>
+	/// <param name="file">The source file in which the abortion is made.</param>
+	/// <param name="line">The line where the abortion is made.</param>
+	extern bool RTEAbortFunc(const char *description, const char *file, int line);
+	extern bool RTEAbortFunc(const std::string description, const char *file, int line);
+#ifdef _WIN32
+  #define RTEAbort(description) {								\
+    if (RTEAbortFunc(description, __FILE__, __LINE__)) {	\
+      __debugbreak();										\
+    }														\
+  }
+#else
+  #define RTEAbort(description){\
+    if (RTEAbortFunc(description, __FILE__, __LINE__)){}\
+  }
+#endif
+	/// <summary>
+	/// A souped-up, customized assert function that brings up a nice dialog box on assert failure.
+	/// The user can choose to break or ignore the particular assertion failure once, or to always ignore.
+	/// </summary>
+	/// <param name="expression">The expression that will be asserted to be true.</param>
+	/// <param name="description">The description of the assertion.</param>
+	/// <param name="file">The source file in which the assertion is made.</param>
+	/// <param name="line">The line where the assertion is made.</param>
+	/// <param name="alwaysIgnore">A reference to a bool that is used in an "Always ignore" functionality.</param>
+	/// <returns>Whether the assertion failed AND the user chose to break in the dialog box.</returns>
+	extern bool RTEAssertFunc(bool expression, const char *description, const char *file, int line, bool &alwaysIgnore);
+	extern bool RTEAssertFunc(bool expression, const std::string description, const char *file, int line, bool &alwaysIgnore);
+#ifdef _WIN32
+  #define RTEAssert(expression, description) {												\
+    static bool alwaysIgnore = false;														\
+    if (!alwaysIgnore) {																	\
+      if (RTEAssertFunc(expression, description, __FILE__, __LINE__, alwaysIgnore)) {		\
+        __debugbreak();																	\
+      }																					\
+    }																						\
+  }
+#else
+  #define RTEAssert(expression, description){\
+    static bool alwaysIgnore = false;\
+    if (!alwaysIgnore){\
+      if(RTEAssertFunc(expression, description, __FILE__, __LINE__, alwaysIgnore)){}\
+    }\
+  }
+#endif
+}
 #endif

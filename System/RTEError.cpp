7cd67c6dcdb5101598b6bc5f4ce90f39361ce443
MaximDude <maximdude@gmail.com>
2020-12-07 15:31:02 +0200

    Remove unnecessary duplicate functions in RTEError and misc changes

--- System/RTEError.cpp
+++ System/RTEError.cpp
@@ -1,5 +1,4 @@
 #include "RTEError.h"
-#include "Constants.h"
 
 namespace RTE {
 
@@ -9,45 +8,38 @@ namespace RTE {
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-	extern bool RTEAbortFunc(const char *description, const char *file, int line) {
+	extern bool RTEAbortFunc(const std::string &description, const std::string &file, int line) {
 		// Save out the screen bitmap, after making a copy of it, faster sometimes
 		if (screen) {
 			BITMAP *abortScreenBuffer = create_bitmap(screen->w, screen->h);
 			blit(screen, abortScreenBuffer, 0, 0, 0, 0, screen->w, screen->h);
 			PALETTE palette;
 			get_palette(palette);
-			save_bmp("abortscreen.bmp", abortScreenBuffer, palette);
+			save_bmp("AbortScreen.bmp", abortScreenBuffer, palette);
 			destroy_bitmap(abortScreenBuffer);
 		}
 		// Ditch the video mode so the message box appears without problems
-		if (screen != 0) { set_gfx_mode(GFX_TEXT, 0, 0, 0, 0); }
-		// Set title of the messagebox
+		if (screen != nullptr) { set_gfx_mode(GFX_TEXT, 0, 0, 0, 0); }
 		set_window_title("RTE Aborted! (x_x)");
 
-		char message[512];
+		std::string abortMessage;
 
 #if defined DEBUG_BUILD || defined MIN_DEBUG_BUILD
 		// Show message box with explanation
-		std::snprintf(message, sizeof(message), "Runtime Error in file %s, line %i, because:\n\n%s\n\nThe last frame has been dumped to 'abortscreen.bmp'", file, line, description);
-		allegro_message(message);
+		abortMessage = "Runtime Error in file " + file + ", line " + std::to_string(line) + ", because:\n\n" + description + "\n\nThe last frame has been dumped to 'AbortScreen.bmp'";
 #else
 		// Shortened and less confusing one. users have no use of knowing which source file and where.
-		std::snprintf(message, sizeof(message), "%s\n\nThe last frame has been dumped to 'abortscreen.bmp'", description);
-		allegro_message(message);
+		abortMessage = description + "\n\nThe last frame has been dumped to 'AbortScreen.bmp'";
 #endif
+		allegro_message(abortMessage.c_str());
+
 		// True so that the debugbreak code is run and the debugger goes there.
 		return true;
 	}
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-	extern bool RTEAbortFunc(const std::string description, const char *file, int line) {
-		return RTEAbortFunc(description.c_str(), file, line);
-	}
-
-/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-	bool RTEAssertFunc(bool expression, const char *description, const char *file, int line, bool &alwaysIgnore) {
+	bool RTEAssertFunc(bool expression, const std::string &description, const std::string &file, int line, bool &alwaysIgnore) {
 		if (!expression) {
 			// TODO: Make this display a box in the game asking whether to ignore or abort. For now, always abort.
 			RTEAbortFunc(description, __FILE__, __LINE__);
@@ -56,11 +48,5 @@ namespace RTE {
 			return true;
 		}
 		return false;
-	}
-
-/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-	extern bool RTEAssertFunc(bool expression, std::string description, const char *file, int line, bool &alwaysIgnore) {
-		return RTEAssertFunc(expression, description.c_str(), file, line, alwaysIgnore);
 	}
 }
--- System/RTEError.h
+++ System/RTEError.h
@@ -32,8 +32,7 @@ namespace RTE {
 	/// <param name="description">The description of the abortion.</param>
 	/// <param name="file">The source file in which the abortion is made.</param>
 	/// <param name="line">The line where the abortion is made.</param>
-	extern bool RTEAbortFunc(const char *description, const char *file, int line);
-	extern bool RTEAbortFunc(const std::string description, const char *file, int line);
+	extern bool RTEAbortFunc(const std::string &description, const std::string &file, int line);
 
 	#define RTEAbort(description) {											\
 		if (RTEAbortFunc(description, __FILE__, __LINE__)) { AbortAction }	\
@@ -49,8 +48,7 @@ namespace RTE {
 	/// <param name="line">The line where the assertion is made.</param>
 	/// <param name="alwaysIgnore">A reference to a bool that is used in an "Always ignore" functionality.</param>
 	/// <returns>Whether the assertion failed AND the user chose to break in the dialog box.</returns>
-	extern bool RTEAssertFunc(bool expression, const char *description, const char *file, int line, bool &alwaysIgnore);
-	extern bool RTEAssertFunc(bool expression, const std::string description, const char *file, int line, bool &alwaysIgnore);
+	extern bool RTEAssertFunc(bool expression, const std::string &description, const std::string &file, int line, bool &alwaysIgnore);
 
 	#define RTEAssert(expression, description) {															\
 		static bool alwaysIgnore = false;																	\

18e7cc2340bea6b94e6486a0a9e77dd07834ed15
MaximDude <maximdude@gmail.com>
2020-12-11 14:25:53 +0200

    Refactor LoadingGUI

--- Menus/LoadingGUI.cpp
+++ Menus/LoadingGUI.cpp
@@ -20,120 +20,119 @@ namespace RTE {
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 	void LoadingGUI::Clear() {
-		m_ControlManager = 0;
-		m_GUIInput = 0;
-		m_GUIScreen = 0;
-		m_LoadingLogWriter = 0;
-		m_LoadingGUIBitmap = 0;
+		m_ControlManager = nullptr;
+		m_GUIInput = nullptr;
+		m_GUIScreen = nullptr;
+		m_LoadingLogWriter = nullptr;
+		m_ProgressListboxBitmap = nullptr;
 		m_PosX = 0;
 		m_PosY = 0;
 	}
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-	//TODO Consider splitting this into a Create method and Init method to standardize LoadingGUI. Need to research if it can viably be done.
 	void LoadingGUI::InitLoadingScreen() {
 		g_FrameMan.LoadPalette("Base.rte/palette.bmp");
-
-		// Create the main GUI
-		m_GUIInput = new AllegroInput(-1);
-		m_GUIScreen = new AllegroScreen(g_FrameMan.GetBackBuffer32());
-
-		// Loading splash screen
 		g_FrameMan.ClearBackBuffer32();
-		SceneLayer *loadingSplash = new SceneLayer();
-		loadingSplash->Create(ContentFile("Base.rte/GUIs/Title/LoadingSplash.png"), false, Vector(), true, false, Vector(1.0, 0));
 
-		// Hardcoded offset to make room for the loading box only if DisableLoadingScreen is false.
-		int loadingSplashOffset = g_SettingsMan.DisableLoadingScreen() ? 14 : 120;
-		loadingSplash->SetOffset(Vector(((loadingSplash->GetBitmap()->w - g_FrameMan.GetResX()) / 2) + loadingSplashOffset, 0));
+		m_GUIInput.reset(new AllegroInput(-1));
+		m_GUIScreen.reset(new AllegroScreen(g_FrameMan.GetBackBuffer32()));
 
-		// Draw onto wrapped strip centered vertically on the screen
-		Box splashBox(Vector(0, (g_FrameMan.GetResY() - loadingSplash->GetBitmap()->h) / 2), g_FrameMan.GetResX(), loadingSplash->GetBitmap()->h);
-		loadingSplash->Draw(g_FrameMan.GetBackBuffer32(), splashBox);
-		delete loadingSplash;
-		loadingSplash = 0;
-
-		g_FrameMan.FlipFrameBuffers();
-
-		if (g_SettingsMan.SettingsNeedOverwrite()) {
-			// Overwrite Settings.ini after all the managers are created to fully populate the file. Up until this moment Settings.ini is populated only with minimal required properties to run.
-			// When the overwrite happens there is a short delay which causes the screen to remain black, so this is done here after the flip to mask that black screen.
-			g_SettingsMan.UpdateSettingsFile();
-		}
-
-		// Set up the loading GUI
 		if (!m_ControlManager) {
-			m_ControlManager = new GUIControlManager();
-
-			// TODO: This should be using the 32bpp main menu skin, but isn't because it needs the config of the base for its listbox
-			// Can get away with this hack for now because the list box that the loading menu uses displays ok when drawn on a 32bpp buffer,
-			// when it's 8bpp internally, since it does not use any masked_blit calls to draw list boxes.
-			// Note also how the GUIScreen passed in here has been created with an 8bpp bitmap, since that is what determines what the GUI manager uses internally
-			if (!m_ControlManager->Create(m_GUIScreen, m_GUIInput, "Base.rte/GUIs/Skins/MainMenu", "LoadingSkin.ini")) {
+			m_ControlManager.reset(new GUIControlManager());
+			if (!m_ControlManager->Create(m_GUIScreen.get(), m_GUIInput.get(), "Base.rte/GUIs/Skins/MainMenu", "LoadingSkin.ini")) {
 				RTEAbort("Failed to create GUI Control Manager and load it from Base.rte/GUIs/Skins/MainMenu/LoadingSkin.ini");
 			}
 			m_ControlManager->Load("Base.rte/GUIs/LoadingGUI.ini");
 		}
+		if (!g_SettingsMan.DisableLoadingScreen()) { CreateProgressReportListbox(); }
 
-		// Place and clear the sectionProgress box
-		dynamic_cast<GUICollectionBox *>(m_ControlManager->GetControl("root"))->SetSize(g_FrameMan.GetResX(), g_FrameMan.GetResY());
-		GUIListBox *listBox = dynamic_cast<GUIListBox *>(m_ControlManager->GetControl("ProgressBox"));
-		// Make the box a bit bigger if there's room in higher, HD resolutions
-		if (g_FrameMan.GetResX() >= 960) {
-			// Make the loading progress box fill the right third of the screen
-			listBox->Resize((g_FrameMan.GetResX() / 3) - 12, listBox->GetHeight());
-			listBox->SetPositionRel(g_FrameMan.GetResX() - listBox->GetWidth() - 12, (g_FrameMan.GetResY() / 2) - (listBox->GetHeight() / 2));
-		} else {
-			// Legacy positioning and sizing when running low resolutions
-			listBox->SetPositionRel(g_FrameMan.GetResX() - listBox->GetWidth() - 12, (g_FrameMan.GetResY() / 2) - (listBox->GetHeight() / 2));
+		SceneLayer loadingSplash;
+		loadingSplash.Create(ContentFile("Base.rte/GUIs/Title/LoadingSplash.png"), false, Vector(), true, false, Vector(1.0F, 0));
+
+		// Hardcoded offset to make room for the loading box only if DisableLoadingScreen is false.
+		int loadingSplashOffset = g_SettingsMan.DisableLoadingScreen() ? 14 : 120;
+		loadingSplash.SetOffset(Vector(static_cast<float>(((loadingSplash.GetBitmap()->w - g_FrameMan.GetResX()) / 2) + loadingSplashOffset), 0));
+
+		// Draw onto wrapped strip centered vertically on the screen
+		Box splashBox(Vector(0, static_cast<float>((g_FrameMan.GetResY() - loadingSplash.GetBitmap()->h) / 2)), static_cast<float>(g_FrameMan.GetResX()), static_cast<float>(loadingSplash.GetBitmap()->h));
+		loadingSplash.Draw(g_FrameMan.GetBackBuffer32(), splashBox);
+
+		g_FrameMan.FlipFrameBuffers();
+
+		// Overwrite Settings.ini after all the managers are created to fully populate the file. Up until this moment Settings.ini is populated only with minimal required properties to run.
+		// When the overwrite happens there is a short delay which causes the screen to remain black, so this is done here after the flip to mask that black screen.
+		if (g_SettingsMan.SettingsNeedOverwrite()) { g_SettingsMan.UpdateSettingsFile(); }
+
+		if (!m_LoadingLogWriter) {
+			m_LoadingLogWriter.reset(new Writer("LogLoading.txt"));
+			if (!m_LoadingLogWriter->WriterOK()) {
+				ShowMessageBox("Failed to instantiate the Loading Log writer!\nModule loading will proceed without being logged!");
+				m_LoadingLogWriter.reset();
+			}
 		}
-		listBox->ClearList();
 
-		// New mechanism to speed up loading times as it turned out that a massive amount of time is spent to update GUI control.
-		if (!g_SettingsMan.DisableLoadingScreen() && !m_LoadingGUIBitmap) {
-			listBox->SetVisible(false);
-			m_LoadingGUIBitmap = create_bitmap_ex(8, listBox->GetWidth(), listBox->GetHeight());
-			clear_to_color(m_LoadingGUIBitmap, 54);
-			rect(m_LoadingGUIBitmap, 0, 0, listBox->GetWidth() - 1, listBox->GetHeight() - 1, 33);
-			rect(m_LoadingGUIBitmap, 1, 1, listBox->GetWidth() - 2, listBox->GetHeight() - 2, 33);
-			m_PosX = listBox->GetXPos();
-			m_PosY = listBox->GetYPos();
-		}
-		// Create the loading log writer
-		if (!m_LoadingLogWriter) { m_LoadingLogWriter = new Writer("LogLoading.txt"); }
-
-		// Load all the data modules
 		LoadDataModules();
 	}
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-	void LoadingGUI::LoadingSplashProgressReport(std::string reportString, bool newItem) {
+	void LoadingGUI::CreateProgressReportListbox() {
+		// Place and clear the sectionProgress box
+		dynamic_cast<GUICollectionBox *>(m_ControlManager->GetControl("root"))->SetSize(g_FrameMan.GetResX(), g_FrameMan.GetResY());
+		GUIListBox *listBox = dynamic_cast<GUIListBox *>(m_ControlManager->GetControl("ProgressBox"));
+
+		// Make the box a bit bigger if there's room in higher, HD resolutions
+		if (g_FrameMan.GetResX() >= 960) { listBox->Resize((g_FrameMan.GetResX() / 3) - 12, listBox->GetHeight()); }
+
+		// Make the loading progress box fill the right third of the screen
+		listBox->SetPositionRel(g_FrameMan.GetResX() - listBox->GetWidth() - 12, (g_FrameMan.GetResY() / 2) - (listBox->GetHeight() / 2));
+		listBox->ClearList();
+
+		if (!m_ProgressListboxBitmap) {
+			listBox->SetVisible(false);
+			m_ProgressListboxBitmap = create_bitmap_ex(8, listBox->GetWidth(), listBox->GetHeight());
+			clear_to_color(m_ProgressListboxBitmap, 54);
+			rect(m_ProgressListboxBitmap, 0, 0, listBox->GetWidth() - 1, listBox->GetHeight() - 1, 33);
+			rect(m_ProgressListboxBitmap, 1, 1, listBox->GetWidth() - 2, listBox->GetHeight() - 2, 33);
+			m_PosX = listBox->GetXPos();
+			m_PosY = listBox->GetYPos();
+		}
+	}
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+	void LoadingGUI::Destroy() {
+		if (m_ProgressListboxBitmap) { destroy_bitmap(m_ProgressListboxBitmap); }
+		Clear();
+	}
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+	void LoadingGUI::LoadingSplashProgressReport(const std::string &reportString, bool newItem) {
 		if (System::IsLoggingToCLI()) { System::PrintLoadingToCLI(reportString, newItem); }
 
-		if (g_LoadingGUI.m_ControlManager) {
-			if (newItem) {
-				// Write out the last line to the log file before starting a new one
-				if (g_LoadingGUI.m_LoadingLogWriter->WriterOK()) { *g_LoadingGUI.m_LoadingLogWriter << reportString << "\n"; }
-				// Scroll bitmap upwards
-				if (g_LoadingGUI.m_LoadingGUIBitmap) { blit(g_LoadingGUI.m_LoadingGUIBitmap, g_LoadingGUI.m_LoadingGUIBitmap, 2, 12, 2, 2, g_LoadingGUI.m_LoadingGUIBitmap->w - 3, g_LoadingGUI.m_LoadingGUIBitmap->h - 12); }
-			}
-			if (g_LoadingGUI.m_LoadingGUIBitmap) {
-				AllegroBitmap bmp(g_LoadingGUI.m_LoadingGUIBitmap);
-				// Clear current line
-				rectfill(g_LoadingGUI.m_LoadingGUIBitmap, 2, g_LoadingGUI.m_LoadingGUIBitmap->h - 12, g_LoadingGUI.m_LoadingGUIBitmap->w - 3, g_LoadingGUI.m_LoadingGUIBitmap->h - 3, 54);
-				// Print new line
-				g_FrameMan.GetSmallFont()->DrawAligned(&bmp, 5, g_LoadingGUI.m_LoadingGUIBitmap->h - 12, reportString.c_str(), GUIFont::Left);
-				// DrawAligned - MaxWidth is useless here, so we're just drawing lines manually
-				vline(g_LoadingGUI.m_LoadingGUIBitmap, g_LoadingGUI.m_LoadingGUIBitmap->w - 2, g_LoadingGUI.m_LoadingGUIBitmap->h - 12, g_LoadingGUI.m_LoadingGUIBitmap->h - 2, 33);
-				vline(g_LoadingGUI.m_LoadingGUIBitmap, g_LoadingGUI.m_LoadingGUIBitmap->w - 1, g_LoadingGUI.m_LoadingGUIBitmap->h - 12, g_LoadingGUI.m_LoadingGUIBitmap->h - 2, 33);
+		if (newItem) {
+			// Write out the last line to the log file before starting a new one and scroll the bitmap upwards
+			if (g_LoadingGUI.m_LoadingLogWriter) { *g_LoadingGUI.m_LoadingLogWriter << reportString << "\n"; }
+			if (g_LoadingGUI.m_ProgressListboxBitmap) { blit(g_LoadingGUI.m_ProgressListboxBitmap, g_LoadingGUI.m_ProgressListboxBitmap, 2, 12, 2, 2, g_LoadingGUI.m_ProgressListboxBitmap->w - 3, g_LoadingGUI.m_ProgressListboxBitmap->h - 12); }
+		}
 
-				// Draw onto current frame buffer
-				blit(g_LoadingGUI.m_LoadingGUIBitmap, g_FrameMan.GetBackBuffer32(), 0, 0, g_LoadingGUI.m_PosX, g_LoadingGUI.m_PosY, g_LoadingGUI.m_LoadingGUIBitmap->w, g_LoadingGUI.m_LoadingGUIBitmap->h);
+		if (g_LoadingGUI.m_ProgressListboxBitmap) {
+			AllegroBitmap drawBitmap(g_LoadingGUI.m_ProgressListboxBitmap);
 
-				g_FrameMan.FlipFrameBuffers();
-			}
+			// Clear current line
+			rectfill(g_LoadingGUI.m_ProgressListboxBitmap, 2, g_LoadingGUI.m_ProgressListboxBitmap->h - 12, g_LoadingGUI.m_ProgressListboxBitmap->w - 3, g_LoadingGUI.m_ProgressListboxBitmap->h - 3, 54);
+			// Print new line
+			g_FrameMan.GetSmallFont()->DrawAligned(&drawBitmap, 5, g_LoadingGUI.m_ProgressListboxBitmap->h - 12, reportString.c_str(), GUIFont::Left);
+			// DrawAligned - MaxWidth is useless here, so we're just drawing lines manually
+			vline(g_LoadingGUI.m_ProgressListboxBitmap, g_LoadingGUI.m_ProgressListboxBitmap->w - 2, g_LoadingGUI.m_ProgressListboxBitmap->h - 12, g_LoadingGUI.m_ProgressListboxBitmap->h - 2, 33);
+			vline(g_LoadingGUI.m_ProgressListboxBitmap, g_LoadingGUI.m_ProgressListboxBitmap->w - 1, g_LoadingGUI.m_ProgressListboxBitmap->h - 12, g_LoadingGUI.m_ProgressListboxBitmap->h - 2, 33);
+
+			// Draw onto current frame buffer
+			blit(g_LoadingGUI.m_ProgressListboxBitmap, g_FrameMan.GetBackBuffer32(), 0, 0, g_LoadingGUI.m_PosX, g_LoadingGUI.m_PosY, g_LoadingGUI.m_ProgressListboxBitmap->w, g_LoadingGUI.m_ProgressListboxBitmap->h);
+
+			g_FrameMan.FlipFrameBuffers();
 		}
 	}
 
@@ -144,6 +143,9 @@ namespace RTE {
 		g_PresetMan.Initialize();
 		ExtractZippedModules();
 		g_PresetMan.LoadAllDataModules();
+
+		// Free any resources used by this because we won't be needing it anymore
+		Destroy();
 
 		return true;
 	}
@@ -280,4 +282,20 @@ namespace RTE {
 			}
 		}
 	}
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+	void LoadingGUI::GUIControlManagerDeleter::operator()(GUIControlManager *ptr) const { ptr->Destroy(); }
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+	void LoadingGUI::AllegroInputDeleter::operator()(AllegroInput *ptr) const { ptr->Destroy(); }
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+	void LoadingGUI::AllegroScreenDeleter::operator()(AllegroScreen *ptr) const { ptr->Destroy(); }
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+	void LoadingGUI::WriterDeleter::operator()(Writer *ptr) const { ptr->EndWrite(); }
 }
--- Menus/LoadingGUI.h
+++ Menus/LoadingGUI.h
@@ -19,25 +19,41 @@ namespace RTE {
 
 	public:
 
+#pragma region Creation
 		/// <summary>
 		/// Constructor method used to instantiate a LoadingGUI object in system memory.
 		/// </summary>
 		LoadingGUI() { Clear(); }
 
 		/// <summary>
-		/// Creates the loading screen and the log writer, then calls loading all the modules.
+		/// Creates the loading screen GUI and the log writer, then proceeds loading all the DataModules.
 		/// </summary>
 		void InitLoadingScreen();
 
+		/// <summary>
+		/// Creates the GUI listbox that the progress report will be drawn to, if not disabled through the settings file to speed up loading times.
+		/// As it turned out, a massive amount of time is spent updating the GUI control and flipping the frame buffers.
+		/// </summary>
+		void CreateProgressReportListbox();
+#pragma endregion
+
+#pragma region Destruction
+		/// <summary>
+		/// Destroys and resets (through Clear()) the LoadingGUI object.
+		/// </summary>
+		void Destroy();
+#pragma endregion
+
+#pragma region Concrete Methods
 		/// <summary>
 		/// Updates the loading progress report.
 		/// </summary>
 		/// <param name="reportString">The string to print in the report and log.</param>
 		/// <param name="newItem">Whether to start a new line in the log writer and to scroll the bitmap.</param>
-		static void LoadingSplashProgressReport(std::string reportString, bool newItem = false);
+		static void LoadingSplashProgressReport(const std::string &reportString, bool newItem = false);
 
 		/// <summary>
-		/// Finding and loading all DataModules
+		/// Finding and loading all DataModules.
 		/// </summary>
 		bool LoadDataModules();
 
@@ -46,27 +62,37 @@ namespace RTE {
 		/// This will cause extracted and available data modules to be updated to whatever is within their corresponding zip files.
 		/// The point of this is that it facilitates downloaded mods being loaded without having to be manually unzipped first by the user.
 		/// </summary>
+		// TODO: Break this down between PresetMan and System.
 		void ExtractZippedModules() const;
+#pragma endregion
 
 	protected:
 
-		static constexpr unsigned int s_MaxFileName = 256; //!< Maximum length of output file directory + name string.
-		static constexpr unsigned int s_FileBufferSize = 8192; //!< Buffer to hold data read from the zip file.
-		static constexpr unsigned int s_MaxUnzippedFileSize = 104857600; //!< Maximum size of single file being extracted from zip archive (100MiB).
+		static constexpr int s_MaxFileName = 512; //!< Maximum length of output file directory + name string.
+		static constexpr int s_FileBufferSize = 8192; //!< Buffer to hold data read from the zip file.
+		static constexpr int s_MaxUnzippedFileSize = 104857600; //!< Maximum size of single file being extracted from zip archive (100MiB).
 
 		/// <summary>
-		/// Array containing all supported file extensions that are allowed to be extracted from zipped module files.
+		/// Container for all the supported file extensions that are allowed to be extracted from zipped module files.
 		/// </summary>
 		const std::unordered_set<std::string> s_SupportedExtensions = { ".ini", ".txt", ".lua", ".cfg", ".bmp", ".png", ".jpg", ".jpeg", ".wav", ".ogg", ".mp3", ".flac" };
 
-		GUIControlManager *m_ControlManager; //!< Manager of the whole LoadingGUI.
-		AllegroInput *m_GUIInput; //!< Input interface of this.
-		AllegroScreen *m_GUIScreen; //!< Screen interface of this.
-		Writer *m_LoadingLogWriter; //!< The Writer that generates the loading log.
-		BITMAP *m_LoadingGUIBitmap; //!< BITMAP that the progress report will be drawn into.
+		/// <summary>
+		/// Custom deleters for std::unique_ptr members. Must be defined to avoid including the class headers and just rely on forward declaration.
+		/// </summary>
+		struct GUIControlManagerDeleter { void operator()(GUIControlManager *ptr) const; };
+		struct AllegroInputDeleter { void operator()(AllegroInput *ptr) const; };
+		struct AllegroScreenDeleter { void operator()(AllegroScreen *ptr) const; };
+		struct WriterDeleter { void operator()(Writer *ptr) const; };
 
-		short m_PosX; //!< Position of the progress report box on X axis.
-		short m_PosY; //!< Position of the progress report box on Y axis.
+		std::unique_ptr<GUIControlManager, GUIControlManagerDeleter> m_ControlManager; //!< Manager of the whole LoadingGUI.
+		std::unique_ptr<AllegroInput, AllegroInputDeleter> m_GUIInput; //!< Input interface of this.
+		std::unique_ptr<AllegroScreen, AllegroScreenDeleter> m_GUIScreen; //!< Screen interface of this.
+		std::unique_ptr<Writer, WriterDeleter> m_LoadingLogWriter; //!< The Writer that generates the loading log.
+		BITMAP *m_ProgressListboxBitmap; //!< BITMAP that the progress report will be drawn into.
+
+		int m_PosX; //!< Position of the progress report box on X axis.
+		int m_PosY; //!< Position of the progress report box on Y axis.
 
 	private:
 

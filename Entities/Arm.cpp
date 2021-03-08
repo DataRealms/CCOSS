ad4ad2aab92e8aa8c3bc456ccbfbf9ea36c9bb0f
fourZK <the4zk@hotmail.com>
2021-03-08 13:23:16 +0200

    Yet another fix for hand drawing

--- Entities/Arm.cpp
+++ Entities/Arm.cpp
@@ -444,7 +444,7 @@ void Arm::UpdateArmFrame() {
 void Arm::Draw(BITMAP *pTargetBitmap, const Vector &targetPos, DrawMode mode, bool onlyPhysical) const {
     Attachable::Draw(pTargetBitmap, targetPos, mode, onlyPhysical);
 
-    if (!onlyPhysical && (mode == g_DrawColor || mode == g_DrawWhite || mode == g_DrawTrans) && (!m_Parent || m_pHeldMO || (!m_pHeldMO && m_DidReach))) {
+    if (!onlyPhysical && (mode == g_DrawColor || mode == g_DrawWhite || mode == g_DrawTrans) && (m_pHeldMO || (!m_pHeldMO && !m_DidReach)) && m_Parent) {
         DrawHand(pTargetBitmap, targetPos, mode);
         if (m_pHeldMO && m_pHeldMO->IsDrawnAfterParent()) { m_pHeldMO->Draw(pTargetBitmap, targetPos, mode, onlyPhysical); }
     }

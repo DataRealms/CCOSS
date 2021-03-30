edc8dd7b2eacda9ba68c2e1a019f6f9f84620f0d
leo <lesgoets@gmail.com>
2021-03-30 19:34:46 +0200

    fixed some msvc bs

--- Main.cpp
+++ Main.cpp
@@ -1828,7 +1828,8 @@ int main(int argc, char **argv) {
 
 #ifdef DEBUG_BUILD
     // Dump out the info about how well memory cleanup went
-    Entity::ClassInfo::DumpPoolMemoryInfo(Writer("MemCleanupInfo.txt"));
+    Writer memCleanup{"MemCleanupInfo.txt"}
+    Entity::ClassInfo::DumpPoolMemoryInfo(memCleanup);
 #endif
 	
     return 0;

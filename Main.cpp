97e40ceb378757241fa685829ada61dee2592c59
MaximDude <maximdude@gmail.com>
2021-04-08 18:10:29 +0300

    Scope methods in Main

--- Main.cpp
+++ Main.cpp
@@ -21,6 +21,7 @@
 #include "GUI.h"
 #include "AllegroInput.h"
 #include "AllegroScreen.h"
+#include "AllegroBitmap.h"
 
 #include "MetaMan.h"
 #include "SettingsMan.h"
@@ -43,10 +44,6 @@ using namespace RTE;
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-/// <summary>
-/// Global variables.
-/// </summary>
-
 volatile bool g_Quit = false;
 bool g_ResetRTE = false; //!< Signals to reset the entire RTE next iteration.
 bool g_InActivity = false;
@@ -57,273 +54,278 @@ int g_StationOffsetY;
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-/// <summary>
-/// This handles when the quit or exit button is pressed on the window.
-/// </summary>
-void QuitHandler(void) { g_Quit = true; }
-END_OF_FUNCTION(QuitHandler)
+namespace RTE {
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-/// <summary>
-/// Reset the current activity.
-/// </summary>
-/// <returns></returns>
-bool ResetActivity() {
-	g_ConsoleMan.PrintString("SYSTEM: Activity was reset!");
-    g_ResetActivity = false;
-
-    // Clear and reset out things
-    g_FrameMan.ClearBackBuffer8();
-    g_FrameMan.FlipFrameBuffers();
-    g_AudioMan.StopAll();
-
-    // Quit if we should
-	if (g_Quit) {
-		return false;
-	}
-
-	// TODO: Deal with GUI resetting here!$@#")
-    // Clear out all MO's
-    g_MovableMan.PurgeAllMOs();
-    // Have to reset TimerMan before creating anything else because all timers are reset against it
-    g_TimerMan.ResetTime();
-
-    g_FrameMan.LoadPalette("Base.rte/palette.bmp");
-    g_FrameMan.FlipFrameBuffers();
-
-    // Reset TimerMan again after loading so there's no residual delay
-    g_TimerMan.ResetTime();
-    // Unpause
-    g_TimerMan.PauseSim(false);
-
-    int error = g_ActivityMan.RestartActivity();
-	if (error >= 0) {
-		g_InActivity = true;
-	} else {
-        // Something went wrong when restarting, so drop out to scenario menu and open the console to show the error messages
-		g_InActivity = false;
-		g_ActivityMan.PauseActivity();
-		g_ConsoleMan.SetEnabled(true);
-		//g_IntroState = MAINTOSCENARIO;
-		return false;
-	}
-    return true;
-}
+	/// <summary>
+	/// This handles when the quit or exit button is pressed on the window.
+	/// </summary>
+	void QuitHandler(void) { g_Quit = true; }
+	END_OF_FUNCTION(QuitHandler)
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-/// <summary>
-/// Start the simulation back up after being paused.
-/// </summary>
-void ResumeActivity() {
-	if (g_ActivityMan.GetActivity()->GetActivityState() != Activity::NotStarted) {
-		g_Quit = false;
-		g_InActivity = true;
-		g_ResumeActivity = false;
+	/// <summary>
+	/// Reset the current activity.
+	/// </summary>
+	/// <returns></returns>
+	bool ResetActivity() {
+		g_ConsoleMan.PrintString("SYSTEM: Activity was reset!");
+		g_ResetActivity = false;
 
+		// Clear and reset out things
 		g_FrameMan.ClearBackBuffer8();
 		g_FrameMan.FlipFrameBuffers();
+		g_AudioMan.StopAll();
+
+		// Quit if we should
+		if (g_Quit) {
+			return false;
+		}
+
+		// TODO: Deal with GUI resetting here!$@#")
+		// Clear out all MO's
+		g_MovableMan.PurgeAllMOs();
+		// Have to reset TimerMan before creating anything else because all timers are reset against it
+		g_TimerMan.ResetTime();
+
 		g_FrameMan.LoadPalette("Base.rte/palette.bmp");
+		g_FrameMan.FlipFrameBuffers();
 
-		g_PerformanceMan.ResetFrameTimer();
+		// Reset TimerMan again after loading so there's no residual delay
+		g_TimerMan.ResetTime();
+		// Unpause
 		g_TimerMan.PauseSim(false);
-		g_ActivityMan.PauseActivity(false);
-	}
-}
 
-/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-/// <summary>
-/// Orders to reset the entire Retro Terrain Engine system next iteration.
-/// </summary>
-void ResetRTE() { g_ResetRTE = true; }
-
-/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-/// <summary>
-/// Indicates whether the system is about to be reset before the next loop starts.
-/// </summary>
-/// <returns>Whether the RTE is about to reset next iteration of the loop or not.</returns>
-bool IsResettingRTE() { return g_ResetRTE; }
-
-/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-/// <summary>
-/// Game menus loop.
-/// </summary>
-/// <returns></returns>
-bool RunMenuLoop() {
-	g_UInputMan.DisableKeys(false);
-	g_UInputMan.TrapMousePos(false);
-
-	// Don't stop the music if reinitializing after a resolution change
-	if (!g_FrameMan.ResolutionChanged()) { g_AudioMan.StopAll(); }
-
-	while (!g_Quit && /*g_IntroState != END &&*/ !g_ResumeActivity) {
-		g_UInputMan.Update();
-		g_TimerMan.Update();
-		g_TimerMan.UpdateSim();
-		g_AudioMan.Update();
-
-		g_MenuMan.Update();
-
-		if (g_NetworkServer.IsServerModeEnabled()) { g_NetworkServer.Update(); }
-
-		g_MenuMan.Draw();
-	}
-	return true;
-}
-
-/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-/// <summary>
-/// Game simulation loop.
-/// </summary>
-bool RunGameLoop() {
-	if (g_Quit) {
+		int error = g_ActivityMan.RestartActivity();
+		if (error >= 0) {
+			g_InActivity = true;
+		} else {
+			// Something went wrong when restarting, so drop out to scenario menu and open the console to show the error messages
+			g_InActivity = false;
+			g_ActivityMan.PauseActivity();
+			g_ConsoleMan.SetEnabled(true);
+			//g_IntroState = MAINTOSCENARIO;
+			return false;
+		}
 		return true;
 	}
-	g_PerformanceMan.ResetFrameTimer();
-	g_TimerMan.PauseSim(false);
-
-	if (g_ResetActivity) { ResetActivity(); }
-
-	while (!g_Quit) {
-		// Need to clear this out; sometimes background layers don't cover the whole back
-		g_FrameMan.ClearBackBuffer8();
-
-		// Update the real time measurement and increment
-		g_TimerMan.Update();
-
-		bool serverUpdated = false;
-
-		// Simulation update, as many times as the fixed update step allows in the span since last frame draw
-		while (g_TimerMan.TimeForSimUpdate()) {
-			serverUpdated = false;
-			g_PerformanceMan.NewPerformanceSample();
-
-			// Advance the simulation time by the fixed amount
-			g_TimerMan.UpdateSim();
-
-			g_PerformanceMan.StartPerformanceMeasurement(PerformanceMan::SimTotal);
-
-			g_UInputMan.Update();
-
-			// It is vital that server is updated after input manager but before activity because input manager will clear received pressed and released events on next update.
-			if (g_NetworkServer.IsServerModeEnabled()) {
-				g_NetworkServer.Update(true);
-				serverUpdated = true;
-			}
-			g_FrameMan.Update();
-			g_LuaMan.Update();
-			g_PerformanceMan.StartPerformanceMeasurement(PerformanceMan::ActivityUpdate);
-			g_ActivityMan.Update();
-			g_PerformanceMan.StopPerformanceMeasurement(PerformanceMan::ActivityUpdate);
-			g_MovableMan.Update();
-            g_AudioMan.Update();
-
-			g_ActivityMan.LateUpdateGlobalScripts();
-
-			g_ConsoleMan.Update();
-			g_PerformanceMan.StopPerformanceMeasurement(PerformanceMan::SimTotal);
-
-			if (!g_InActivity) {
-				g_TimerMan.PauseSim(true);
-				// If we're not in a metagame, then show main menu
-				if (g_MetaMan.GameInProgress()) {
-					//g_IntroState = CAMPAIGNFADEIN;
-				} else {
-					const Activity *activity = g_ActivityMan.GetActivity();
-					// If we edited something then return to main menu instead of scenario menu player will probably switch to area/scene editor.
-					if (activity && activity->GetPresetName() == "None") {
-						//g_IntroState = MENUAPPEAR;
-					} else {
-						//g_IntroState = MAINTOSCENARIO;
-					}
-				}
-				RunMenuLoop();
-			}
-			// Resetting the simulation
-			if (g_ResetActivity) {
-				// Reset and quit if user quit during reset loading
-				if (!ResetActivity()) { break; }
-			}
-			// Resuming the simulation
-			if (g_ResumeActivity) { ResumeActivity(); }
-		}
-
-		if (g_NetworkServer.IsServerModeEnabled()) {
-			// Pause sim while we're waiting for scene transmission or scene will start changing before clients receive them and those changes will be lost.
-			if (!g_NetworkServer.ReadyForSimulation()) {
-				g_TimerMan.PauseSim(true);
-			} else {
-				if (g_InActivity) { g_TimerMan.PauseSim(false); }
-			}
-			if (!serverUpdated) {
-				g_NetworkServer.Update();
-				serverUpdated = true;
-			}
-			if (g_NetworkServer.GetServerSimSleepWhenIdle()) {
-				long long ticksToSleep = g_TimerMan.GetTimeToSleep();
-				if (ticksToSleep > 0) {
-					double secsToSleep = static_cast<double>(ticksToSleep) / static_cast<double>(g_TimerMan.GetTicksPerSecond());
-					long long milisToSleep = static_cast<long long>(secsToSleep) * 1000;
-					std::this_thread::sleep_for(std::chrono::milliseconds(milisToSleep));
-				}
-			}
-		}
-		g_FrameMan.Draw();
-		g_FrameMan.FlipFrameBuffers();
-	}
-	return true;
-}
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-/// <summary>
-/// Command-line argument handling.
-/// </summary>
-/// <param name="argCount">Argument count.</param>
-/// <param name="argValue">Argument values.</param>
-void HandleMainArgs(int argCount, char **argValue) {
-	// Discard the first argument because it's always the executable path/name
-	argCount--;
-	argValue++;
-	if (argCount == 0) {
-		return;
+	/// <summary>
+	/// Start the simulation back up after being paused.
+	/// </summary>
+	void ResumeActivity() {
+		if (g_ActivityMan.GetActivity()->GetActivityState() != Activity::NotStarted) {
+			g_Quit = false;
+			g_InActivity = true;
+			g_ResumeActivity = false;
+
+			g_FrameMan.ClearBackBuffer8();
+			g_FrameMan.FlipFrameBuffers();
+			g_FrameMan.LoadPalette("Base.rte/palette.bmp");
+
+			g_PerformanceMan.ResetFrameTimer();
+			g_TimerMan.PauseSim(false);
+			g_ActivityMan.PauseActivity(false);
+		}
 	}
-	bool launchModeSet = false;
-	bool singleModuleSet = false;
 
-	for (int i = 0; i < argCount;) {
-		std::string currentArg = argValue[i];
-		bool lastArg = i + 1 == argCount;
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-		// Print loading screen console to cout
-		if (currentArg == "-cout") { System::EnableLoggingToCLI(); }
+	/// <summary>
+	/// Orders to reset the entire Retro Terrain Engine system next iteration.
+	/// </summary>
+	void ResetRTE() { g_ResetRTE = true; }
 
-		// Load a single module right after the official modules
-		if (!lastArg && !singleModuleSet && currentArg == "-module") {
-			std::string moduleToLoad = argValue[++i];
-			if (moduleToLoad.find(System::GetModulePackageExtension()) == moduleToLoad.length() - System::GetModulePackageExtension().length()) {
-				g_PresetMan.SetSingleModuleToLoad(moduleToLoad);
-				singleModuleSet = true;
-			}
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+	/// <summary>
+	/// Indicates whether the system is about to be reset before the next loop starts.
+	/// </summary>
+	/// <returns>Whether the RTE is about to reset next iteration of the loop or not.</returns>
+	bool IsResettingRTE() { return g_ResetRTE; }
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+	/// <summary>
+	/// Game menus loop.
+	/// </summary>
+	/// <returns></returns>
+	bool RunMenuLoop() {
+		g_UInputMan.DisableKeys(false);
+		g_UInputMan.TrapMousePos(false);
+
+		// Don't stop the music if reinitializing after a resolution change
+		if (!g_FrameMan.ResolutionChanged()) { g_AudioMan.StopAll(); }
+
+		while (!g_Quit && /*g_IntroState != END &&*/ !g_ResumeActivity) {
+			g_UInputMan.Update();
+			g_TimerMan.Update();
+			g_TimerMan.UpdateSim();
+			g_AudioMan.Update();
+
+			g_MenuMan.Update();
+
+			if (g_NetworkServer.IsServerModeEnabled()) { g_NetworkServer.Update(); }
+
+			g_MenuMan.Draw();
 		}
-		if (!launchModeSet) {
-			// Launch game in server mode
-			if (currentArg == "-server") {
-				g_NetworkServer.EnableServerMode();
-				g_NetworkServer.SetServerPort(!lastArg ? argValue[++i] : "8000");
-				launchModeSet = true;
-			// Launch game directly into editor activity
-			} else if (!lastArg && currentArg == "-editor") {
-				g_MenuMan.SetEditorToLaunch(argValue[++i]);
-				launchModeSet = true;
-			}
+		return true;
+	}
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+	/// <summary>
+	/// Game simulation loop.
+	/// </summary>
+	bool RunGameLoop() {
+		if (g_Quit) {
+			return true;
 		}
-		++i;
+		g_PerformanceMan.ResetFrameTimer();
+		g_TimerMan.PauseSim(false);
+
+		if (g_ResetActivity) { ResetActivity(); }
+
+		while (!g_Quit) {
+			// Need to clear this out; sometimes background layers don't cover the whole back
+			g_FrameMan.ClearBackBuffer8();
+
+			// Update the real time measurement and increment
+			g_TimerMan.Update();
+
+			bool serverUpdated = false;
+
+			// Simulation update, as many times as the fixed update step allows in the span since last frame draw
+			while (g_TimerMan.TimeForSimUpdate()) {
+				serverUpdated = false;
+				g_PerformanceMan.NewPerformanceSample();
+
+				// Advance the simulation time by the fixed amount
+				g_TimerMan.UpdateSim();
+
+				g_PerformanceMan.StartPerformanceMeasurement(PerformanceMan::SimTotal);
+
+				g_UInputMan.Update();
+
+				// It is vital that server is updated after input manager but before activity because input manager will clear received pressed and released events on next update.
+				if (g_NetworkServer.IsServerModeEnabled()) {
+					g_NetworkServer.Update(true);
+					serverUpdated = true;
+				}
+				g_FrameMan.Update();
+				g_LuaMan.Update();
+				g_PerformanceMan.StartPerformanceMeasurement(PerformanceMan::ActivityUpdate);
+				g_ActivityMan.Update();
+				g_PerformanceMan.StopPerformanceMeasurement(PerformanceMan::ActivityUpdate);
+				g_MovableMan.Update();
+				g_AudioMan.Update();
+
+				g_ActivityMan.LateUpdateGlobalScripts();
+
+				g_ConsoleMan.Update();
+				g_PerformanceMan.StopPerformanceMeasurement(PerformanceMan::SimTotal);
+
+				if (!g_InActivity) {
+					g_TimerMan.PauseSim(true);
+					// If we're not in a metagame, then show main menu
+					if (g_MetaMan.GameInProgress()) {
+						//g_IntroState = CAMPAIGNFADEIN;
+					} else {
+						const Activity *activity = g_ActivityMan.GetActivity();
+						// If we edited something then return to main menu instead of scenario menu player will probably switch to area/scene editor.
+						if (activity && activity->GetPresetName() == "None") {
+							//g_IntroState = MENUAPPEAR;
+						} else {
+							//g_IntroState = MAINTOSCENARIO;
+						}
+					}
+					RunMenuLoop();
+				}
+				// Resetting the simulation
+				if (g_ResetActivity) {
+					// Reset and quit if user quit during reset loading
+					if (!ResetActivity()) { break; }
+				}
+				// Resuming the simulation
+				if (g_ResumeActivity) { ResumeActivity(); }
+			}
+
+			if (g_NetworkServer.IsServerModeEnabled()) {
+				// Pause sim while we're waiting for scene transmission or scene will start changing before clients receive them and those changes will be lost.
+				if (!g_NetworkServer.ReadyForSimulation()) {
+					g_TimerMan.PauseSim(true);
+				} else {
+					if (g_InActivity) { g_TimerMan.PauseSim(false); }
+				}
+				if (!serverUpdated) {
+					g_NetworkServer.Update();
+					serverUpdated = true;
+				}
+				if (g_NetworkServer.GetServerSimSleepWhenIdle()) {
+					long long ticksToSleep = g_TimerMan.GetTimeToSleep();
+					if (ticksToSleep > 0) {
+						double secsToSleep = static_cast<double>(ticksToSleep) / static_cast<double>(g_TimerMan.GetTicksPerSecond());
+						long long milisToSleep = static_cast<long long>(secsToSleep) * 1000;
+						std::this_thread::sleep_for(std::chrono::milliseconds(milisToSleep));
+					}
+				}
+			}
+			g_FrameMan.Draw();
+			g_FrameMan.FlipFrameBuffers();
+		}
+		return true;
+	}
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+	/// <summary>
+	/// Command-line argument handling.
+	/// </summary>
+	/// <param name="argCount">Argument count.</param>
+	/// <param name="argValue">Argument values.</param>
+	void HandleMainArgs(int argCount, char **argValue) {
+		// Discard the first argument because it's always the executable path/name
+		argCount--;
+		argValue++;
+		if (argCount == 0) {
+			return;
+		}
+		bool launchModeSet = false;
+		bool singleModuleSet = false;
+
+		for (int i = 0; i < argCount;) {
+			std::string currentArg = argValue[i];
+			bool lastArg = i + 1 == argCount;
+
+			// Print loading screen console to cout
+			if (currentArg == "-cout") { System::EnableLoggingToCLI(); }
+
+			// Load a single module right after the official modules
+			if (!lastArg && !singleModuleSet && currentArg == "-module") {
+				std::string moduleToLoad = argValue[++i];
+				if (moduleToLoad.find(System::GetModulePackageExtension()) == moduleToLoad.length() - System::GetModulePackageExtension().length()) {
+					g_PresetMan.SetSingleModuleToLoad(moduleToLoad);
+					singleModuleSet = true;
+				}
+			}
+			if (!launchModeSet) {
+				// Launch game in server mode
+				if (currentArg == "-server") {
+					g_NetworkServer.EnableServerMode();
+					g_NetworkServer.SetServerPort(!lastArg ? argValue[++i] : "8000");
+					launchModeSet = true;
+					// Launch game directly into editor activity
+				} else if (!lastArg && currentArg == "-editor") {
+					g_MenuMan.SetEditorToLaunch(argValue[++i]);
+					launchModeSet = true;
+				}
+			}
+			++i;
+		}
 	}
 }
 

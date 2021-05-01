58fe74b5302b5c16a8a56b1b5f1c3736c8b5d77a
HeliumAnt <lesgoets@gmail.com>
2021-05-02 01:26:48 +0200

    add case sensitivity to exposed lua file checks

--- Managers/LuaMan.cpp
+++ Managers/LuaMan.cpp
@@ -2647,6 +2647,12 @@ int LuaMan::RunScriptFile(const std::string &filePath, bool consoleErrors) {
         m_LastError = "Can't run a script file with an empty filepath!";
         return -1;
     }
+
+	if (!System::PathExistsCaseSensitive(filePath)){
+		m_LastError = "Script file doesn't exist!";
+		return -1;
+	}
+
     int error = 0;
 
     lua_pushcfunction(m_pMasterState, &AddFileAndLineToError);
@@ -2738,6 +2744,10 @@ int LuaMan::FileOpen(std::string filename, std::string mode)
 
 	// Do not open paths with '..'
 	if (fullPath.find(dotString) != string::npos)
+		return -1;
+
+	// Do not open paths that aren't written correctly
+	if (!System::PathExistsCaseSensitive(std::filesystem::path(fullPath).lexically_normal().generic_string()))
 		return -1;
 
 	// Allow to edit files only inside .rte folders

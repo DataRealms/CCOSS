0bd0882f3f087db490a94384202bf504bd3b2d80
Johnathon Selstad <makeshifted@gmail.com>
2021-09-05 19:11:03 -0700

    Expose the Traveling Flag Properly

--- Entities/MovableObject.h
+++ Entities/MovableObject.h
@@ -1806,8 +1806,7 @@ enum MOType
 
 	void SetProvidesPieMenuContext(bool value) { m_ProvidesPieMenuContext = value; }
 
-// Set to true while travelling; prevents self-intersection
-bool m_tempDisableGettingHit;
+    bool GetTraveling() const { return GetRootParent()->m_tempDisableGettingHit; }
 
 //////////////////////////////////////////////////////////////////////////////////////////
 // Protected member variable and method declarations
@@ -1939,6 +1938,8 @@ protected:
     bool m_ToDelete;
     // To draw this guy's HUD or not
     bool m_HUDVisible;
+    // Set to true while travelling; prevents self-intersection
+    bool m_tempDisableGettingHit;
 
     std::map<std::string, bool> m_AllLoadedScripts; //!< A map of script paths to the enabled state of the given script.
     std::unordered_map<std::string, std::vector<std::string>> m_FunctionsAndScripts; //!< A map of function names to vectors of scripts paths. Used to maintain script execution order and avoid extraneous Lua calls.
--- Managers/SceneMan.cpp
+++ Managers/SceneMan.cpp
@@ -615,7 +615,7 @@ MOID SceneMan::GetMOIDPixel(int pixelX, int pixelY)
     MOID moid = getpixel(m_pMOIDLayer->GetBitmap(), pixelX, pixelY);
     if (moid != g_NoMOID && moid != g_MOIDMaskColor) {
         MOSprite* mo = dynamic_cast<MOSprite*>(g_MovableMan.GetMOFromID(moid));
-        return (mo && !mo->GetRootParent()->m_tempDisableGettingHit) ? moid : g_NoMOID;
+        return (mo && !mo->GetTraveling()) ? moid : g_NoMOID;
     } else {
         return g_NoMOID;
     }

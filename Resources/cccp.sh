ba0e97ba6eeb9b863d044ef23558a91a1dcb82e2
HeliumAnt <lesgoets@gmail.com>
2021-09-09 19:09:43 +0200

    fix run script configuration

--- Resources/cccp.sh
+++ Resources/cccp.sh
@@ -1,39 +1,40 @@
 #! /bin/bash
 
-link_base_files()
-{
-		local base_data_path=@BASEDATAPATH@
-		if [[ ! -d base_data_path ]]; then
-				exit 1
-		fi
+link_base_files() {
+	if [[ ! -d $base_data_path ]]; then
+		exit 1
+	fi
 
-		ln -s "${base_data_path}"/*.rte "${base_data_path}/Credits.txt" $tmp_dir
+	ln -s "${base_data_path}"/*.rte "${base_data_path}/Credits.txt" $tmp_dir
 }
 
-link_user_files()
-{
-		local user_files=("LogConsole.txt" "LogLoading.txt" "LogLoadingWarnings.txt" "AbortScreen.bmp" "AbortScreen.png" "Settings.ini")
-		local user_directories=("Metagames.rte" "Scenes.rte" "_ScreenShots")
+link_user_files() {
+	local user_files=("LogConsole.txt" "LogLoading.txt" "LogLoadingWarnings.txt" "AbortScreen.bmp" "AbortScreen.png" "Settings.ini")
+	local user_directories=("Metagames.rte" "Scenes.rte" "_ScreenShots")
 
-		if [[! -e "${user_data}"]]
-			 mkdir -p "${user_data}"
+	if [[ ! -d "${user_data}" ]]; then
+		mkdir -p "${user_data}"
+	fi
+
+	for file in $user_files; do
+		ln -s "${user_data}/$file" $tmp_dir
+	done
+
+	for directory in $user_directories; do
+		if [[ ! -e "${user_data}/${directory}" ]]; then
+			cp -r "${base_data_path}/${directory}" "${user_data}/"
 		fi
-
-		for file in $user_files; do
-				ln -s "${user_data}/$file" $tmp_dir
-		done
-
-		for directory in $user_directories; do
-				if [[! -e "${user_data}/${directory}"]]; then
-						cp -r "${base_data_path}/${directory}" "${user_data}/"
-				fi
-				ln -sf "${user_data}/${directory}" "${tmp_dir}/"
-		done
+		ln -sf "${user_data}/${directory}" "${tmp_dir}/"
+	done
 }
 
-tmp_dir=$(mktemp -d CCCP.XXXXXXXXX)
+tmp_dir=$(mktemp -d "/tmp/CCCP.XXXXXXXXX")
+base_data_path=@BASEDATAPATH@
 user_local_home=${XG_DATA_HOME:-~/".local/share"}
 user_data="${user_local_home}/Cortex Commad"
+
+link_base_files
+link_user_files
 
 cd "${tmp_dir}"
 

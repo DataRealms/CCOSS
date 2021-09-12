6c74160cc5933e15927bfb3fdb2a88cf98621ceb
Gareth YR <garethrickwood@hotmail.com>
2021-09-12 13:17:09 -0300

    Fix pie menu issues with editors

--- Menus/AssemblyEditorGUI.h
+++ Menus/AssemblyEditorGUI.h
@@ -175,7 +175,7 @@ public:
 // Arguments:       None.
 // Return value:    The enum'd int of any slice activated. See the PieSliceIndex enum.
 
-    PieSlice::PieSliceIndex GetActivatedPieSlice();
+    PieSlice::PieSliceIndex GetActivatedPieSlice() { return m_ActivatedPieSliceType; }
 
 
 //////////////////////////////////////////////////////////////////////////////////////////
@@ -383,6 +383,7 @@ protected:
 
     // The pie menu
     PieMenuGUI *m_pPieMenu;
+    PieSlice::PieSliceIndex m_ActivatedPieSliceType; //!< The activated PieSliceType, reset every frame.
     // The object picker
     ObjectPickerGUI *m_pPicker;
     // The ID of the DataModule that contains the native Tech of the Player using this menu

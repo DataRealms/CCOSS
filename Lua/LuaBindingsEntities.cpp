b764b1b5b14014d6db037c58514c7190323cc0bc
Gareth YR <garethrickwood@hotmail.com>
2021-09-14 17:27:19 -0300

    Moved lua bindings into LuaBindingsEntities

--- Lua/LuaBindingsEntities.cpp
+++ Lua/LuaBindingsEntities.cpp
@@ -364,6 +364,8 @@ namespace RTE {
 		.property("EmitAngle", &AEmitter::GetEmitAngle, &AEmitter::SetEmitAngle)
 		.property("GetThrottle", &AEmitter::GetThrottle, &AEmitter::SetThrottle)
 		.property("Throttle", &AEmitter::GetThrottle, &AEmitter::SetThrottle)
+		.property("MinThrottleRangeTODORENAME", &AEmitter::GetMinThrottleRange, &AEmitter::SetMinThrottleRange)
+		.property("MaxThrottleRangeTODORENAME", &AEmitter::GetMaxThrottleRange, &AEmitter::SetMaxThrottleRange)
 		.property("BurstSpacing", &AEmitter::GetBurstSpacing, &AEmitter::SetBurstSpacing)
 		.property("BurstDamage", &AEmitter::GetBurstDamage, &AEmitter::SetBurstDamage)
 		.property("EmitterDamageMultiplier", &AEmitter::GetEmitterDamageMultiplier, &AEmitter::SetEmitterDamageMultiplier)

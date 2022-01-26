b98a7cbad088d980ff135c9c5f686c5cd2364925
Gareth YR <garethrickwood@hotmail.com>
2022-01-26 17:19:11 -0400

    Significantly reworked PieSlice so it's read from INI
    Added flag for whether it can be the middle slice of a pie quadrant
    Added pointer to its original source for easy removal later
    Added concept of slot counts for how big this slice is
    Swapped to global directions enum
    
    # Conflicts:
    #	System/PieSlice.cpp - Solved using mine
    #	System/PieSlice.h - Solved using mine but with semicolons added as per theirs

--- System/PieSlice.h
+++ System/PieSlice.h
@@ -1,20 +1,25 @@
 #ifndef _RTEPIESLICE_
 #define _RTEPIESLICE_
 
-#include "Serializable.h"
+#include "Constants.h"
+
 #include "Icon.h"
 
 namespace RTE {
 
+	class PieMenuGUI;
+	class Actor;
+
 	/// <summary>
-	/// An individual slice in a pie menu.
+	/// An individual PieSlice in a PieMenu.
 	/// </summary>
-	class PieSlice : public Serializable {
+	class PieSlice : public Entity {
 
 	public:
 
+		EntityAllocation(PieSlice);
 		SerializableOverrideMethods;
-		SerializableClassNameGetter;
+		ClassInfoGetters;
 
 		/// <summary>
 		/// Enumeration for the types of PieSlices.
@@ -31,10 +36,12 @@ namespace RTE {
 			PSI_BUYMENU,
 			PSI_FULLINVENTORY,
 			PSI_STATS,
-			PSI_MINIMAP,
-			PSI_FORMSQUAD,
+			PSI_MAP,
 			PSI_CEASEFIRE,
+			// Squad
+			PSI_FORMSQUAD,
 			// AI Modes
+			PSI_AIMODES,
 			PSI_SENTRY,
 			PSI_PATROL,
 			PSI_BRAINHUNT,
@@ -60,21 +67,7 @@ namespace RTE {
 			PSI_TEAM2,
 			PSI_TEAM3,
 			PSI_TEAM4,
-			// Custom
-			PSI_SCRIPTED,
-			// How many pie slice types there are
 			PSI_COUNT
-		};
-
-		/// <summary>
-		/// An enum for the directions a slice can be placed in, in a given pie menu.
-		/// </summary>
-		enum SliceDirection {
-			NONE = 0,
-			UP,
-			RIGHT,
-			DOWN,
-			LEFT
 		};
 
 #pragma region Creation
@@ -82,16 +75,6 @@ namespace RTE {
 		/// Constructor method used to instantiate a PieSlice object in system memory. Create() should be called before using the object.
 		/// </summary>
 		PieSlice() { Clear(); }
-
-		/// <summary>
-		/// Constructor method used to instantiate a PieSlice object in system memory. Create() should be called before using the object.
-		/// </summary>
-		/// <param name="description">The description of the pie slice.</param>
-		/// <param name="sliceType">The type of the pie slice.</param>
-		/// <param name="direction">The direction of the pie slice.</param>
-		/// <param name="enabled">Whether the slice is enabled. Defaults to true.</param>
-		PieSlice(const std::string_view &description, PieSliceIndex sliceType, SliceDirection direction, bool enabled = true) { Clear(); m_Description = description; m_SliceType = sliceType; m_Direction = direction; m_Enabled = enabled; }
-		//PieSlice(const std::string &description, PieSliceIndex sliceType, SliceDirection direction, bool enabled = true) : Clear(), m_Description(description), m_SliceType(sliceType), m_Direction(direction), m_Enabled(enabled) { Clear(); }
 
 		/// <summary>
 		/// Makes the PieSlice object ready for use.
@@ -105,15 +88,6 @@ namespace RTE {
 		/// <param name="reference">A reference to the PieSlice to deep copy.</param>
 		/// <returns>An error return value signaling success or any particular failure. Anything below 0 is an error signal.</returns>
 		int Create(const PieSlice &reference);
-
-		/// <summary>
-		/// Makes the Serializable ready for use. Pie slices being read from a file will always be scripted SliceType.
-		/// </summary>
-		/// <param name="reader">A Reader that the Serializable will create itself from.</param>
-		/// <param name="checkType">Whether there is a class name in the stream to check against to make sure the correct type is being read from the stream.</param>
-		/// <param name="doCreate">Whether to do any additional initialization of the object after reading in all the properties from the Reader. This is done by calling Create().</param>
-		/// <returns>An error return value signaling success or any particular failure. Anything below 0 is an error signal.</returns>
-		int Create(Reader &reader, bool checkType = true, bool doCreate = true) override { m_SliceType = PieSliceIndex::PSI_SCRIPTED; return Serializable::Create(reader, checkType, doCreate); }
 #pragma endregion
 
 #pragma region Destruction
@@ -125,28 +99,52 @@ namespace RTE {
 
 #pragma region Getters and Setters
 		/// <summary>
-		/// Gets the type of this PieSlice.
+		/// Gets the Type of this PieSlice.
 		/// </summary>
-		/// <returns>The type of this PieSlice.</returns>
-		const PieSliceIndex & GetType() const { return m_SliceType; }
+		/// <returns>The Type of this PieSlice.</returns>
+		PieSliceIndex GetType() const { return m_Type; }
 
 		/// <summary>
-		/// Gets the description this PieSlice shows when selected in its pie menu.
+		/// Sets the Type of this PieSlice.
 		/// </summary>
-		/// <returns>The description of this PieSlice.</returns>
-		const std::string & GetDescription() const { return m_Description; }
+		/// <param name="newType">The new Type of this PieSlice.</param>
+		void SetType(PieSliceIndex newType) { m_Type = newType; }
 
 		/// <summary>
-		/// Gets the direction this PieSlice is set to be at in its pie menu.
+		/// Gets the Direction of this PieSlice.
 		/// </summary>
-		/// <returns>The direction of this PieSlice.</returns>
-		const SliceDirection & GetDirection() const { return m_Direction; }
+		/// <returns>The Direction of this PieSlice.</returns>
+		Directions GetDirection() const { return m_Direction; }
 
 		/// <summary>
-		/// Sets the direction this PieSlice should be at in its pie menu.
+		/// Sets the Direction of this PieSlice.
 		/// </summary>
-		/// <param name="direction">The direction to set for the PieSlice.</param>
-		void SetDirection(SliceDirection direction) { m_Direction = direction; }
+		/// <param name="newDirection">The new Direction of this PieSlice.</param>
+		void SetDirection(Directions newDirection) { m_Direction = newDirection; }
+
+		/// <summary>
+		/// Gets whether or not this PieSlice can be the middle PieSlice of a PieQuadrant.
+		/// </summary>
+		/// <returns>Whether or not this PieSlice can be the middle PieSlice of a PieQuadrant.</returns>
+		bool GetCanBeMiddleSlice() const { return m_CanBeMiddleSlice; }
+
+		/// <summary>
+		/// Sets whether or not this PieSlice can be the middle PieSlice of a PieQuadrant.
+		/// </summary>
+		/// <param name="newCanBeMiddleSlice">Whether or not this PieSlice can be the middle PieSlice of a PieQuadrant.</param>
+		void SetCanBeMiddleSlice(bool newCanBeMiddleSlice) { m_CanBeMiddleSlice = newCanBeMiddleSlice; }
+
+		/// <summary>
+		/// Gets the original Entity source of this PieSlice, if there is one.
+		/// </summary>
+		/// <returns>A pointer to the original Entity source of this PieSlice, if there is one.</returns>
+		const Entity * GetOriginalSource() const { return m_OriginalSource; }
+
+		/// <summary>
+		/// Sets the original Entity source of this PieSlice.
+		/// </summary>
+		/// <param name="originalSource">A pointer to the original Entity source of this PieSlice.</param>
+		void SetOriginalSource(const Entity *originalSource) { m_OriginalSource = originalSource; }
 
 		/// <summary>
 		/// Gets whether or not this PieSlice is enabled.
@@ -164,7 +162,7 @@ namespace RTE {
 		/// Gets whether or not this PieSlice has a valid Icon.
 		/// </summary>
 		/// <returns>Whether or not this PieSlice has a valid Icon.</returns>
-		bool HasIcon() const { return m_Icon.GetFrameCount() > 0; }
+		bool HasIcon() const { return m_Icon && m_Icon->GetFrameCount() > 0; }
 
 		/// <summary>
 		/// Gets the appropriate icon frame for this PieSlice.
@@ -174,16 +172,35 @@ namespace RTE {
 		BITMAP * GetAppropriateIcon(bool sliceIsSelected = false) const;
 
 		/// <summary>
+		/// Sets the new Icon for this PieSlice. Ownership IS transferred.
+		/// </summary>
+		/// <param name="newIcon">The new Icon for this PieSlice.</param>
+		void SetIcon(Icon *newIcon) { m_Icon = std::unique_ptr<Icon>(newIcon); }
+
+		/// <summary>
 		/// Gets the file path of the scripted file this PieSlice should run when activated. Empty if it's not a scripted type. Empty if its SliceType isn't scripted.
 		/// </summary>
 		/// <returns>The file path to the script file this PieSlice should load when activated.</returns>
-		const std::string &GetScriptPath() const { return m_ScriptPath; }
+		const std::string & GetScriptPath() const { return m_ScriptPath; }
 
 		/// <summary>
 		/// Gets the name of the Lua function to run when this PieSlice is activated as a scripted pie menu option. Empty if its SliceType isn't scripted.
 		/// </summary>
 		/// <returns>The Lua function name this PieSlice should execute when activated.</returns>
-		const std::string &GetFunctionName() const { return m_FunctionName; }
+		const std::string & GetFunctionName() const { return m_FunctionName; }
+
+		//TODO Ideally this would be done with a weak_ptr but I'm not sure how it'll go with LuaMan. Try it out and see
+		/// <summary>
+		/// Gets the sub-PieMenu for this PieSlice if there is one. Ownership is NOT transferred.
+		/// </summary>
+		/// <returns>The sub-PieMenu for this PieSlice if there is one. Ownership is NOT transferred.</returns>
+		PieMenuGUI * GetSubMenu() const { return m_SubMenu.get(); }
+
+		/// <summary>
+		/// Sets the sub-PieMenu for this PieSlice. Ownership IS transferred.
+		/// </summary>
+		/// <param name="pieMenuGUI">The new sub-PieMenu for this PieSlice. Ownership IS transferred.</param>
+		void SetSubMenu(PieMenuGUI *pieMenuGUI) { m_SubMenu = std::unique_ptr<PieMenuGUI>(pieMenuGUI); }
 #pragma endregion
 
 #pragma region Angle Getter and Setters
@@ -191,25 +208,25 @@ namespace RTE {
 		/// Gets the start angle this PieSlice's area is set to be at in its pie menu.
 		/// </summary>
 		/// <returns>The start angle of this PieSlice's area.</returns>
-		float GetAreaStart() const { return m_AreaStart; }
+		float GetStartAngle() const { return m_StartAngle; }
 
 		/// <summary>
 		/// Sets the start angle this PieSlice's area should be at in its pie menu.
 		/// </summary>
-		/// <param name="areaStart">The start angle to set for the PieSlice's area.</param>
-		void SetAreaStart(float areaStart) { m_AreaStart = areaStart; }
+		/// <param name="startAngle">The start angle to set for the PieSlice's area.</param>
+		void SetStartAngle(float startAngle) { m_StartAngle = startAngle; RecalculateMidAngle(); }
 
 		/// <summary>
-		/// Gets the arc length this of this PieSlice's area.
+		/// Gets the number of slots this PieSlice takes up.
 		/// </summary>
-		/// <returns>The arc length of this PieSlice's area.</returns>
-		float GetAreaArc() const { return m_AreaArc; }
+		/// <returns>The number of slots this PieSlice takes up.</returns>
+		int GetSlotCount() const { return m_SlotCount; }
 
 		/// <summary>
-		/// Sets the arc length of this PieSlice's area.
+		/// Sets the number of slots this PieSlice takes up.
 		/// </summary>
-		/// <param name="areaArc">The arc length to set for the PieSlice's area.</param>
-		void SetAreaArc(float areaArc) { m_AreaArc = areaArc; }
+		/// <param name="slotCount">The number of slots this PieSlice should take up.</param>
+		void SetSlotCount(int slotCount) { m_SlotCount = std::max(1, slotCount); RecalculateMidAngle(); }
 
 		/// <summary>
 		/// Gets the mid angle this PieSlice's area is set to be at in its pie menu.
@@ -225,21 +242,29 @@ namespace RTE {
 #pragma endregion
 
 	private:
-		
-		static const std::string c_ClassName; //!< ClassName for PieSlice.
 
-		PieSliceIndex m_SliceType; //!< The PieSlice type, also serves as icon index.
-		std::string m_Description; //!< Description of what this slice option does.
-		SliceDirection m_Direction; //!< The desired direction/location of this on the pie menu.
-		bool m_Enabled; //!< Whether this slice is enabled or disabled and grayed out.
-		Icon m_Icon; //!< The icon of this pie slice.
+		static Entity::ClassInfo m_sClass; //!< ClassInfo for this class.
 
-		std::string m_ScriptPath; //!< Path to the script file this should run if this is a scripted selection.
-		std::string m_FunctionName; //!< Name of the function in the script that this should run if a scripted pie option.
+		PieSliceIndex m_Type; //!< The slice type, also used to determine the icon.
+		Directions m_Direction; //!< The desired direction/location of this on the PieMenu.
+		bool m_CanBeMiddleSlice; //!< Whether or not this PieSlice is allowed to be the middle slice. Defaults to true and should usually stay that way.
+		const Entity *m_OriginalSource; //!< A pointer to the original source of this PieSlice, normally filled in when PieSlices are added to PieMenus by objects other than the PieMenu's owner, and nullptr otherwise.
 
-		float m_AreaStart; //!< The start angle of this' area on the pie menu, counted in radians from straight out right and going counter clockwise.
-		float m_AreaArc; //!< The arc length of the slice area, so that the icon should be drawn at the areastart + halfway of this.
-		float m_MidAngle; //!< Mid angle, basically m_AreaStart + (m_AreaArc / 2).
+		bool m_Enabled; //!< Whether this PieSlice is enabled or disabled and greyed out.
+		std::unique_ptr<Icon> m_Icon; //!< The icon of this PieSlice.
+
+		std::string m_ScriptPath; //!< Path to the script file this should run when activated.
+		std::string m_FunctionName; //!< Name of the function in the script this should run when activated.
+		std::unique_ptr<PieMenuGUI> m_SubMenu; //!< Unique pointer to the sub-PieMenu this should open when activated.
+
+		float m_StartAngle; //!< The start angle of this PieSlice's area on the PieMenu, counted in radians from straight out right and going counter clockwise.
+		int m_SlotCount; //!< The arc length of the PieSlice area, so that the icon should be drawn at the AreaStart + halfway of this.
+		float m_MidAngle; //!< The mid angle of this PieSlice.
+
+		/// <summary>
+		/// Recalculates this PieSlice's mid angle based on its start angle and slot count.
+		/// </summary>
+		void RecalculateMidAngle();
 
 		/// <summary>
 		/// Clears all the member variables of this PieSlice, effectively resetting the members of this abstraction level only.

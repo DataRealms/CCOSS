2f33aa551d8b9ae213c682d96986086728707d8d
Gareth YR <garethrickwood@hotmail.com>
2022-02-02 00:50:02 -0400

    Forgot to stage some rewrites to the rewrite

--- Menus/MainMenuGUI.cpp
+++ Menus/MainMenuGUI.cpp
@@ -222,8 +222,8 @@ namespace RTE {
 
 		std::string metaNotice = {
 			"- A T T E N T I O N -\n\n"
-			"The Conquest mode is very INCOMPLETE and flawed, and we will be redoing it from the ground-up in future.\n"
-			"For now though, it's fully playable, and you can absolutely enjoy fighting the A.I. and/or up to three friends in co-op, 2 vs 2, etc.\n\n"
+			"Please note that Conquest mode is very INCOMPLETE and flawed, and the Cortex Command Community Project team will be remaking it from the ground-up in future. "
+			"For now though, it's fully playable, and you can absolutely enjoy playing it with the A.I. and/or up to three friends. Alternatively, check out Void Wanderers on cccp.mod.io!\n\n"
 			"Also, if you have not yet played Cortex Command, we recommend you first try the tutorial:"
 		};
 		metaNoticeLabel->SetText(metaNotice);

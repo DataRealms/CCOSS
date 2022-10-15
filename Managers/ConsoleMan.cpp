5f109cd8eb25b2cf99ab0b885a2b121c9397c11b
HeliumAnt <lesgoets@gmail.com>
2022-10-16 00:08:49 +0200

    fix keycodes

--- Managers/ConsoleMan.cpp
+++ Managers/ConsoleMan.cpp
@@ -227,11 +227,11 @@ namespace RTE {
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 	void ConsoleMan::Update() {
-		if (g_UInputMan.FlagCtrlState() && g_UInputMan.KeyPressed(SDLK_BACKQUOTE)) {
+		if (g_UInputMan.FlagCtrlState() && g_UInputMan.KeyPressed(SDL_SCANCODE_GRAVE)) {
 			SetReadOnly();
 		}
 
-		if (!g_UInputMan.FlagShiftState() && (!g_UInputMan.FlagCtrlState() && g_UInputMan.KeyPressed(SDLK_BACKQUOTE))) {
+		if (!g_UInputMan.FlagShiftState() && (!g_UInputMan.FlagCtrlState() && g_UInputMan.KeyPressed(SDL_SCANCODE_GRAVE))) {
 			if (IsEnabled()) {
 				if (!m_ReadOnly) {
 					m_InputTextBox->SetEnabled(false);
@@ -266,9 +266,9 @@ namespace RTE {
 
 		m_GUIControlManager->Update();
 
-		if (g_UInputMan.FlagCtrlState() && g_UInputMan.KeyPressed(SDLK_DOWN)) {
+		if (g_UInputMan.FlagCtrlState() && g_UInputMan.KeyPressed(SDL_SCANCODE_DOWN)) {
 			SetConsoleScreenSize(m_ConsoleScreenRatio + 0.05F);
-		} else if (g_UInputMan.FlagCtrlState() && g_UInputMan.KeyPressed(SDLK_UP)) {
+		} else if (g_UInputMan.FlagCtrlState() && g_UInputMan.KeyPressed(SDL_SCANCODE_UP)) {
 			SetConsoleScreenSize(m_ConsoleScreenRatio - 0.05F);
 		}
 
@@ -277,9 +277,9 @@ namespace RTE {
 			m_InputTextBox->SetFocus();
 
 			if (!m_InputLog.empty() && !g_UInputMan.FlagCtrlState()) {
-				if (g_UInputMan.KeyPressed(SDLK_UP)) {
+				if (g_UInputMan.KeyPressed(SDL_SCANCODE_UP)) {
 					LoadLoggedInput(false);
-				} else if (g_UInputMan.KeyPressed(SDLK_DOWN)) {
+				} else if (g_UInputMan.KeyPressed(SDL_SCANCODE_DOWN)) {
 					LoadLoggedInput(true);
 				}
 			}
@@ -291,7 +291,7 @@ namespace RTE {
 		}
 
 		// Execute string when Enter is pressed, or execute immediately if a newline character is found, meaning multiple strings were pasted in.
-		if ((g_UInputMan.KeyPressed(SDLK_RETURN) || g_UInputMan.KeyPressed(SDLK_KP_ENTER)) || (m_InputTextBox->GetText().find_last_of('\n') != std::string::npos)) {
+		if ((g_UInputMan.KeyPressed(SDLK_RETURN) || g_UInputMan.KeyPressed(SDL_SCANCODE_KP_ENTER)) || (m_InputTextBox->GetText().find_last_of('\n') != std::string::npos)) {
 			FeedString(m_InputTextBox->GetText().empty() ? true : false);
 		}
 	}

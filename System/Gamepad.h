5180076fa443806c9c2cb3b0b81e762d78ded034
HeliumAnt <lesgoets@gmail.com>
2022-10-16 00:02:19 +0200

    add digital axis

--- System/Gamepad.h
+++ System/Gamepad.h
@@ -6,21 +6,27 @@
 namespace RTE {
 	struct Gamepad {
 	public:
-		Gamepad() = default;
-		Gamepad(SDL_JoystickID id, size_t numAxis, size_t numButtons):m_JoystickID{id}, m_Axis(numAxis),m_Buttons(numButtons) {}
+		Gamepad():m_DeviceIndex(-1), m_JoystickID(-1) {};
+		Gamepad(int deviceIndex, SDL_JoystickID id, size_t numAxis, size_t numButtons) :
+		    m_DeviceIndex(deviceIndex), m_JoystickID{id}, m_Axis(numAxis), m_DigitalAxis(numAxis), m_Buttons(numButtons) {}
 		~Gamepad() = default;
 
+		int m_DeviceIndex;
 		SDL_JoystickID m_JoystickID;
-		std::vector<Vector> m_Axis;
+		std::vector<int> m_Axis;
+		std::vector<int> m_DigitalAxis;
 		std::vector<bool> m_Buttons;
 
-		bool operator==(const Gamepad& rhs) {
+		bool operator==(int joystickID) {
+			return m_JoystickID == joystickID;
+		}
+		bool operator==(const Gamepad &rhs) {
 			return m_JoystickID == rhs.m_JoystickID;
 		}
-		bool operator<(const Gamepad& rhs) {
+		bool operator<(const Gamepad &rhs) {
 			return m_JoystickID < rhs.m_JoystickID;
 		}
 	};
-}
+} // namespace RTE
 
 #endif

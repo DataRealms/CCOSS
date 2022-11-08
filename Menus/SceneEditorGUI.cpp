69b6d6adb9418b8be22d2d54840eb92eb7feb560
Gareth YR <garethrickwood@hotmail.com>
2022-11-08 13:25:05 -0400

    Fixed some pie menu issues with changing team in the scene editor (completely forgot to test this lol, thanks Causeless!)
    Also fixed some potential ownership issues with sub piemenus

--- Menus/SceneEditorGUI.cpp
+++ Menus/SceneEditorGUI.cpp
@@ -501,6 +501,10 @@ void SceneEditorGUI::Update()
 			m_PlaceTeam = Activity::TeamOne;
 		} else if (m_PieMenu->GetPieCommand() == PieSlice::SliceType::EditorTeam2) {
 			m_PlaceTeam = Activity::TeamTwo;
+		} else if (m_PieMenu->GetPieCommand() == PieSlice::SliceType::EditorTeam3) {
+			m_PlaceTeam = Activity::TeamThree;
+		} else if (m_PieMenu->GetPieCommand() == PieSlice::SliceType::EditorTeam4) {
+			m_PlaceTeam = Activity::TeamFour;
 			// Toggle between normal scene object editing, and AI plan editing
 		} else if (m_PieMenu->GetPieCommand() == PieSlice::SliceType::Map) {
 			SetFeatureSet(m_FeatureSet == FeatureSets::ONLOADEDIT ? FeatureSets::AIPLANEDIT : FeatureSets::ONLOADEDIT);

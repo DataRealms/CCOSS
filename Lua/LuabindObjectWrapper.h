2987817983af0c26571bea6ca680667e96fb96d7
Gareth YR <garethrickwood@hotmail.com>
2022-11-15 22:31:52 -0400

    Added a filepath field to LuabindObjectWrapper for ease
    Got rid of unnecessary destroy method to just delete the objects in the destructor instead
    Removed setters since the wrapper probably shouldn't be modified after it's made, without first deleting the luabind::object (so what's the point of modifying it)

--- Lua/LuabindObjectWrapper.h
+++ Lua/LuabindObjectWrapper.h
@@ -23,38 +23,34 @@ namespace RTE {
 		/// <summary>
 		/// Constructor method used to instantiate a LuabindObjectWrapper object in system memory.
 		/// </summary>
-		explicit LuabindObjectWrapper(luabind::adl::object *luabindObject) : m_LuabindObject(luabindObject) {}
+		explicit LuabindObjectWrapper(luabind::adl::object *luabindObject, const std::string_view &filePath) : m_LuabindObject(luabindObject), m_FilePath(filePath) {}
 #pragma endregion
 
 #pragma region Destruction
 		/// <summary>
 		/// Destructor method used to clean up a LuabindObjectWrapper object before deletion from system memory.
 		/// </summary>
-		~LuabindObjectWrapper() { Destroy(); }
-
-		/// <summary>
-		/// Destroys and resets (through Clear()) the LuabindObjectWrapper object.
-		/// </summary>
-		void Destroy();
+		~LuabindObjectWrapper();
 #pragma endregion
 
-#pragma region Getters and Setters
+#pragma region Getters
 		/// <summary>
-		/// Gets the wrapper's luabind object. Ownership is NOT transferred!
+		/// Gets the LuabindObjectWrapper's luabind object. Ownership is NOT transferred!
 		/// </summary>
-		/// <returns>The wrapper's luabind object.</returns>
+		/// <returns>The LuabindObjectWrapper's luabind object.</returns>
 		luabind::adl::object * GetLuabindObject() const { return m_LuabindObject; }
 
 		/// <summary>
-		/// Sets the wrapper's luabind object. Ownership is transferred!
+		/// Gets the LuabindObjectWrapper's file path.
 		/// </summary>
-		/// <param name="newLuabindObject">The new luabind object for the wrapper.</param>
-		void SetLuabindObject(luabind::adl::object *newLuabindObject) { m_LuabindObject = newLuabindObject; }
+		/// <returns>The LuabindObjectWrapper's file path.</returns>
+		const std::string & GetFilePath() const { return m_FilePath; }
 #pragma endregion
 
 	private:
 
 		luabind::adl::object *m_LuabindObject; //!< The luabind object this is wrapping.
+		std::string m_FilePath; //!< The filepath the wrapped luabind object represents, if it's a function.
 	};
 }
 #endif

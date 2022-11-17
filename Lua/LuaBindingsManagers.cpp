447e4cf8ead28bb349ea8d2533dfc7a05586876b
Fris0uman <sthapplz@gmail.com>
2022-11-17 13:56:00 +0100

    format

--- Lua/LuaBindingsManagers.cpp
+++ Lua/LuaBindingsManagers.cpp
@@ -191,7 +191,7 @@ namespace RTE {
 		.def("ReloadEntityPreset", ReloadEntityPreset1)
 		.def("ReloadEntityPreset", ReloadEntityPreset2)
 		.def("ReloadAllScripts", &PresetMan::ReloadAllScripts)
-    .def("IsModuleOfficial", &PresetMan::IsModuleOfficial)
+		.def("IsModuleOfficial", &PresetMan::IsModuleOfficial)
 		.def("FullModulePath", &PresetMan::FullModulePath);
 	}
 

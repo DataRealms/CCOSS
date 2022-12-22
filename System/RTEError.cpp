02f0000cba1c080c1cc721b8d40939aa815424aa
Tommy March <tommymarch5@gmail.com>
2022-12-22 00:10:52 +0000

    If we're aborting, attempt to save the game.
    Use static to avoid re-entrancy.
    
    Use std::abort() instead of std::exit() to better handle aborts in background threads.

--- System/RTEError.cpp
+++ System/RTEError.cpp
@@ -1,5 +1,8 @@
 #include "RTEError.h"
 
+// For saving our game on failure
+#include "ActivityMan.h"
+
 namespace RTE {
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -9,6 +12,16 @@ namespace RTE {
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 	void RTEAbortFunc(const std::string &description, const std::string &file, int line) {
+		// We attempt to save the game when aborting, but this could potentially lead to a recursive fault if the saving itself is aborting
+		// So ensure we're not recursing back in here
+		static bool currentAborting = false;
+		if (currentAborting) {
+			// Crap, we're going in a loop. Just ignore all future aborts and let the saving hopefully complete, until the stack unrolls
+			return;
+		}
+
+		currentAborting = true;
+
 		// Save out the screen bitmap, after making a copy of it, faster sometimes
 		if (screen) {
 			BITMAP *abortScreenBuffer = create_bitmap(screen->w, screen->h);
@@ -18,6 +31,7 @@ namespace RTE {
 			save_bmp("AbortScreen.bmp", abortScreenBuffer, palette);
 			destroy_bitmap(abortScreenBuffer);
 		}
+
 		// Ditch the video mode so the message box appears without problems
 		if (screen != nullptr) { set_gfx_mode(GFX_TEXT, 0, 0, 0, 0); }
 		set_window_title("RTE Aborted! (x_x)");
@@ -25,9 +39,13 @@ namespace RTE {
 		std::string abortMessage;
 
 		// Show message box with explanation
-		abortMessage = "Runtime Error in file " + file + ", line " + std::to_string(line) + ", because:\n\n" + description + "\n\nThe last frame has been dumped to 'AbortScreen.bmp'";
+		abortMessage = "Runtime Error in file " + file + ", line " + std::to_string(line) + ", because:\n\n" + description + "\n\nThe game has attempted to save to 'AbortSave'.'";
 		ShowMessageBox(abortMessage);
 
+		// Attempt to save the game itself, so the player can hopefully resume where they were
+		g_ActivityMan.SaveCurrentGame("AbortSave");
+
+		currentAborting = false;
 		AbortAction;
 	}
 

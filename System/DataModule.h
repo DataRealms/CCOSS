b1761667aac62cd264421483295d9653ee36332d
Stuart <stuart1230@gmail.com>
2023-01-01 13:40:49 +1030

    style: update `m_SupportedGameVersion` property documentation comment

--- System/DataModule.h
+++ System/DataModule.h
@@ -321,7 +321,7 @@ namespace RTE {
 		std::string m_ScriptPath; //!< Path to script to execute when this module is loaded.
 		bool m_IsFaction; //!< Whether this data module is considered a faction.
 		bool m_IsMerchant; //!< Whether this data module is considered a merchant.
-		version::Semver200_version m_SupportedGameVersion; //!< Game version this DataModule supports. Needs to match exactly for this DataModule to be allowed. Base DataModules don't need this.
+		version::Semver200_version m_SupportedGameVersion; //!< Game version this DataModule supports. Needs to satisfy Caret Version Range for this DataModule to be allowed. Base DataModules don't need this.
 		int m_Version; //!< Version number, starting with 1.
 		int m_ModuleID; //!< ID number assigned to this upon loading, for internal use only, don't reflect in ini's.
 

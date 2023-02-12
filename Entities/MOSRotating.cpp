b1d8e2544db501c3dbd30cf4335e661838e5476b
Causeless <tommymarch5@gmail.com>
2023-02-12 23:46:57 +0000

    Whoops

--- Entities/MOSRotating.cpp
+++ Entities/MOSRotating.cpp
@@ -1596,7 +1596,7 @@ void MOSRotating::Update() {
 
     for (auto woundItr = m_Wounds.begin(); woundItr != m_Wounds.end(); ) {
         AEmitter* wound = *woundItr;
-        ++woundItr
+        ++woundItr;
         RTEAssert(wound && wound->IsAttachedTo(this), "Broken wound AEmitter in Update");
         wound->Update();
     }

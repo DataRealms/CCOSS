e9a265a93df620c843196add80dafb6f5d266be0
Gareth YR <garethrickwood@hotmail.com>
2023-05-24 11:17:27 -0300

    Added missing safety check

--- Entities/AHuman.cpp
+++ Entities/AHuman.cpp
@@ -3411,8 +3411,10 @@ void AHuman::Update()
 			}
 		}
 	} else if (m_ArmsState == THROWING_RELEASE && m_ThrowTmr.GetElapsedSimTimeMS() > 100) {
-		m_pFGArm->SetHeldDevice(dynamic_cast<HeldDevice *>(SwapNextInventory()));
-		m_pFGArm->SetHandPos(m_Pos + RotateOffset(m_HolsterOffset));
+		if (m_pFGArm) {
+			m_pFGArm->SetHeldDevice(dynamic_cast<HeldDevice *>(SwapNextInventory()));
+			m_pFGArm->SetHandPos(m_Pos + RotateOffset(m_HolsterOffset));
+		}
 		EquipShieldInBGArm();
 		m_ArmsState = WEAPON_READY;
 	} else if (m_ArmsState == THROWING_RELEASE) {

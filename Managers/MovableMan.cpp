c68ad0d3a8b9b34b9e27cc9e6b97e50d055b488c
Causeless <tommymarch5@gmail.com>
2023-06-19 16:20:03 +0100

    Fixed issue where actors without lua state (i.e brain case) had no pie menu

--- Managers/MovableMan.cpp
+++ Managers/MovableMan.cpp
@@ -2000,6 +2000,13 @@ void MovableMan::UpdateControllers()
                 }
                 g_LuaMan.SetThreadLuaStateOverride(nullptr);
             });
+
+        // Update actors without any lua state 
+        for (Actor* actor : m_Actors) {
+            if (actor->GetLuaState() == nullptr) {
+                actor->GetController()->Update();
+            }
+        }
     }
     g_PerformanceMan.StopPerformanceMeasurement(PerformanceMan::ActorsAI);
 }

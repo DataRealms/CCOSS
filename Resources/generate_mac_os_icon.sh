ecc52378215cae855f1674406a22ce13aa3935db
traunts <73866773+traunts@users.noreply.github.com>
2023-08-29 15:06:39 +0930

    refactor: remove whitespace from icon generation script

--- Resources/generate_mac_os_icon.sh
+++ Resources/generate_mac_os_icon.sh
@@ -3,7 +3,6 @@ mkdir ccicon.iconset
 sips -z 16 16     $1 --out ccicon.iconset/icon_16x16.png
 sips -z 32 32     $1 --out ccicon.iconset/icon_16x16@2x.png
 sips -z 32 32     $1 --out ccicon.iconset/icon_32x32.png
-
 sips -z 64 64     $1 --out ccicon.iconset/icon_32x32@2x.png
 sips -z 128 128   $1 --out ccicon.iconset/icon_128x128.png
 sips -z 256 256   $1 --out ccicon.iconset/icon_128x128@2x.png

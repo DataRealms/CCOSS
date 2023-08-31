23a672132cdd0f19d689f7c74d54c4e0be5ef92e
Causeless <tommymarch5@gmail.com>
2023-08-31 23:41:57 +0100

    Allow having only synced update

--- Entities/MovableObject.cpp
+++ Entities/MovableObject.cpp
@@ -1052,7 +1052,7 @@ int MovableObject::UpdateScripts(ThreadScriptsToRun scriptsToRun) {
         // TODO - in future, enforce that everything in MultiThreaded Update() is const, so non-const actions must be performed in SyncedUpdate
         // This would require a bunch of Lua binding fuckery, but eventually maybe it'd be possible.
         // I wonder if we can do some SFINAE magic to make the luabindings automagically do a no-op with const objects, to avoid writing the bindings twice
-        if (status >= 0 && scriptsToRun == ThreadScriptsToRun::SingleThreaded) {
+        if (status >= -1 && scriptsToRun == ThreadScriptsToRun::SingleThreaded) {
             // If we're in a SingleThreaded context, we run the MultiThreaded scripts synced updates:
             status = RunScriptedFunctionInAppropriateScripts("SyncedUpdate", false, true, {}, {}, ThreadScriptsToRun::Both);
         }
